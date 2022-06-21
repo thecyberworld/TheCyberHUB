82f250289bb45ad78476acac2a457c0f590d4cbc
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-21 20:18:30 +0530

    Create Hero.jsx

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import Video from '../../assets/videos/video.mp4';
+
+import {
+    HeroContainer,
+    HeroBg,
+    VideoBg,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight
+} from './HeroElements';
+import {Button} from '../ButtonElements';
+
+
+const Hero = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
+
+    return (
+        <HeroContainer id={"home"}>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type={'video/mp4'}/>
+            </HeroBg>
+
+            <HeroContent>
+                <HeroH1> Cyber Security Made Easy. </HeroH1>
+                <HeroP>
+                    Cyber Security is a field that is growing at an exponential rate.
+                </HeroP>
+
+                <HeroBtnWrapper>
+                    <Button to={"signup"} onMouseEnter={onHover} onMouseLeave={onHover}
+                    primary={"true"} dark={"true"}>
+                        Get Started {hover ? <ArrowForward/> : <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    );
+};
+
+export default Hero;
