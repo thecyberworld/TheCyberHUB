c27116fd139c2d8bea72b262468bbcc472b95e27
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-22 22:09:06 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
+import {IconContext} from 'react-icons/lib';
+import {animateScroll as scroll} from 'react-scroll';
 import {FaBars} from 'react-icons/fa';
 import {
     Nav,
@@ -13,44 +15,61 @@ import {
 } from "./NavbarElements";
 
 const Navbar = ({toggle}) => {
+    const [scrollNav, setScrollNav] = useState(false);
+
+    const changeNav = () => {
+        if (window.scrollY >= 80) {
+            setScrollNav(true);
+        } else {
+            setScrollNav(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav);
+    }, []);
+
+
     return (
         <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to={"/"}>Thecyberworld</NavLogo>
-                    <MobileIcon onClick={toggle}>
-                        <FaBars/>
-                    </MobileIcon>
-                    <NavMenu>
-                        <NavItem>
-                            <NavLinks to={"about"}>About</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to={"freeCourses"}>Free Courses</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to={"community"}>Community</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to={"services"}>Services</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to={"Testimonials"}>Testimonials</NavLinks>
-                        </NavItem>
+            <IconContext.Provider value={{color: '#fff'}}>
+                <Nav scrollNav={scrollNav}>
+                    <NavbarContainer>
+                        <NavLogo to={"/"}>Thecyberworld</NavLogo>
+                        <MobileIcon onClick={toggle}>
+                            <FaBars/>
+                        </MobileIcon>
+                        <NavMenu>
+                            <NavItem>
+                                <NavLinks to={"about"}>About</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to={"freeCourses"}>Free Courses</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to={"community"}>Community</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to={"services"}>Services</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to={"Testimonials"}>Testimonials</NavLinks>
+                            </NavItem>
 
-                        <NavItem>
-                            <NavLinks to={"newsletters"}>Newsletters</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to={"socials"}>Socials</NavLinks>
-                        </NavItem>
-                    </NavMenu>
+                            <NavItem>
+                                <NavLinks to={"newsletters"}>Newsletters</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to={"socials"}>Socials</NavLinks>
+                            </NavItem>
+                        </NavMenu>
 
-                    <NavBtn>
-                        <NavBtnLink to={"/subscribe"} onClick={toggle}>Subscribe</NavBtnLink>
-                    </NavBtn>
-                </NavbarContainer>
-            </Nav>
+                        <NavBtn>
+                            <NavBtnLink to={"/subscribe"} onClick={toggle}>Subscribe</NavBtnLink>
+                        </NavBtn>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
         </>
     );
 };
