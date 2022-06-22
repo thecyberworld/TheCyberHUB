bae724b161ebe3916ef30ea58843192f7f24bb44
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-22 19:37:08 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export {default as Navbar} from './Navbar/Navbar'
 export {default as Sidebar} from './Sidebar/Sidebar'
 export {default as Info} from './Info/Info'
+export {default as Services} from './Services/Services'
 
 export {default as Hero} from './Hero/Hero'
 export {default as Loader} from './Loader'
