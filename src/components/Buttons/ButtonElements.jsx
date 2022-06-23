e5ba97caa8ae0cf866f4b6cc59ca28a9fbe899e7
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-23 08:22:36 +0530

    Update ButtonElements.jsx

--- src/components/Buttons/ButtonElements.jsx
+++ src/components/Buttons/ButtonElements.jsx
@@ -22,3 +22,26 @@ export const Button = styled(ScrollLink)`
     background: ${({primary}) => primary ? '#fff' : '#01BF71'};
   }
 `
+
+export const ButtonLink = styled.a`
+  border-radius: 10px;
+  margin-right: 20px; //border-radius: 50px;
+  background: ${({primary}) => primary ? '#01BF71' : '#010606'};
+  white-space: nowrap;
+  padding: ${({big}) => (big ? '14px 48px' : '12px 30px')};
+  color: ${({dark}) => dark ? '#010606' : '#fff'};
+  font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
+  outline: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: ${({primary}) => primary ? '#fff' : '#01BF71'};
+  }
+`
