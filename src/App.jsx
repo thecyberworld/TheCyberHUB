693a8a098c68597a52e9b86c665e6974f30590b1
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-04 08:55:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 import './App.css';
 
 import Homepage from './Pages/Homepage';
-import {Footer} from "./components";
 
 const App = () => {
     return (
         <>
             <Homepage/>
-            <Footer/>
         </>
     );
 };
