570909d1e402ee00d59b82563876800cc39d807e
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-04 09:05:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 
-import Homepage from './Pages/Homepage';
+import Homepage from './pages/Homepage';
 import {Footer} from "./components";
 import ScrollToTop from "./components/ScrollToTop";
 
