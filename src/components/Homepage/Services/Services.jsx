d21c9a5f2a26d66fe3f1862e796cf2df1610bf3a
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-16 06:45:17 +0530

    Update Services.jsx

--- src/components/Homepage/Services/Services.jsx
+++ src/components/Homepage/Services/Services.jsx
@@ -12,34 +12,35 @@ import {
 import Icon1 from '../../../assets/images/Servies/undraw_video_files_fu10.svg';
 import Icon2 from '../../../assets/images/Servies/undraw_community_re_cyrm.svg';
 import Icon3 from '../../../assets/images/Servies/undraw_version_control_re_mg66.svg';
+import Icon4 from '../../../assets/images/open-source-contribution.svg';
 import {ScrollText} from "./ServicesElements";
 
 const Services = () => {
     return (
         <ServicesContainer id="services">
-            <ServicesH1>Our Services</ServicesH1>
+            <ServicesH1>Our Focus</ServicesH1>
             <ServicesWrapper>
-                <ScrollText to={"courses"} smooth={true} duration={500} spy={true} exact={"true"} offset={-80}>
+                <ScrollText to={"contribute"} smooth={true} duration={500} spy={true} exact={"true"} offset={-80}>
                     <ServicesCard>
-                        <ServicesIcon src={Icon1}/>
-                        <ServicesH2>Free Courses</ServicesH2>
-                        <ServicesP>Free courses that will help you to start with cyber security.</ServicesP>
-                    </ServicesCard>
-                </ScrollText>
-
-                <ScrollText to={"community"} smooth={true} duration={500} spy={true} exact={"true"} offset={-80}>
-                    <ServicesCard>
-                        <ServicesIcon src={Icon2}/>
-                        <ServicesH2>Community Support</ServicesH2>
-                        <ServicesP>We have a Cyber-security community.</ServicesP>
+                        <ServicesIcon src={Icon4}/>
+                        <ServicesH2>Open Source</ServicesH2>
+                        <ServicesP>We have many Open Source projects.</ServicesP>
                     </ServicesCard>
                 </ScrollText>
 
                 <ScrollText to={"contribute"} smooth={true} duration={500} spy={true} exact={"true"} offset={-80}>
                     <ServicesCard>
                         <ServicesIcon src={Icon3}/>
-                        <ServicesH2>Open Source</ServicesH2>
-                        <ServicesP>We have open-source projects, where you can contribute to it.</ServicesP>
+                        <ServicesH2>Contribute</ServicesH2>
+                        <ServicesP>Contribute to the Open Source.</ServicesP>
+                    </ServicesCard>
+                </ScrollText>
+
+                <ScrollText to={"community"} smooth={true} duration={500} spy={true} exact={"true"} offset={-80}>
+                    <ServicesCard>
+                        <ServicesIcon src={Icon2}/>
+                        <ServicesH2>Community</ServicesH2>
+                        <ServicesP>Join Cyber Security and Open Source communities.</ServicesP>
                     </ServicesCard>
                 </ScrollText>
             </ServicesWrapper>
