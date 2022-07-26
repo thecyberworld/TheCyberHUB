8782f15e2a17ae1b407c452b39d53a3884e3f9f6
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-26 08:01:59 +0530

    fix: courses (#65)
    * feat: courses
    
    * fix: courses

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 
 import Homepage from './pages/Homepage';
+import Courses from './pages/Courses';
 import {Footer} from "./components";
 import ScrollToTop from "./components/ScrollToTop";
 import {Navigate} from "react-router";
---
+++ src/pages/Courses.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import YoutubeCoursesNavbar from "../components/Courses/YoutubeCoursesNavbar/YoutubeCoursesNavbar";
+import YoutubeCourses from "../components/Courses/YoutubeCourses";
+
+const Courses = () => {
+    return (
+        <div>
+            <YoutubeCoursesNavbar/>
+            <YoutubeCourses/>
+        </div>
+    );
+};
+
+export default Courses;
