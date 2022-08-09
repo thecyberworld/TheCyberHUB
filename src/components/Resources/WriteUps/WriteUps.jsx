7b7b3fc23ba9ab1f0a8c827813d2429a8dcf93b3
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-08-09 22:45:23 +0530

    feat: writeUps under construction

--- src/components/Resources/WriteUps/WriteUps.jsx
+++ src/components/Resources/WriteUps/WriteUps.jsx
@@ -1,14 +1,68 @@
 import React from 'react';
-import {Section} from "./WriteUpsElements";
+import {Section, WriteUpSection, AuthorSection, AuthorImage, AuthorUsername, Date, ContentBody, Content, Heading, Paragraph, ImageSection, Image, ContentFooter, Tag, MinRead, Bookmark, MarginTop, HR} from './WriteUpsElements';
 
+import {MdOutlineBookmarkAdd, MdBookmarkAdded, MdOutlineBookmarkRemove} from 'react-icons/md';
+import {BsFillBookmarkXFill} from 'react-icons/bs';
+import {AiOutlineShareAlt} from "react-icons/all";
 
-const WriteUps = () => {
+export const WriteUps = () => {
     return (
-        <Section>
-            <h1>
-                Under Development
-            </h1>
-        </Section>
+        <WriteUpSection>
+            <Section>
+                <h1>
+                    Under Development
+                </h1>
+            </Section>
+            <AuthorSection>
+                <AuthorImage src="https://miro.medium.com/fit/c/176/176/1*WrWKJOzibjROkAF5S5Qn8w.jpeg"
+                             alt={"Steiner255"}/>
+                <AuthorUsername>
+                    Steiner255
+                </AuthorUsername>
+                <Date>
+                    5 Days Ago
+                </Date>
+            </AuthorSection>
+            <MarginTop/>
+
+            <ContentBody>
+                <Content>
+                    <Heading>
+                        Another day, Another IDOR vulnerability — $5000 Reddit Bug Bounty
+                    </Heading>
+                    <MarginTop/>
+                    <Paragraph>
+                        Gaining unprivileged access to Reddit moderator logs — Here we go. Again. IDOR, or insecure
+                        direct object reference, is a common yet insecure....
+                    </Paragraph>
+                </Content>
+                <ImageSection>
+                    <Image src="https://miro.medium.com/fit/c/224/224/1*cRu3ETnHdt6n-z96oedxPQ.png" alt="Bootcamp"/>
+                </ImageSection>
+            </ContentBody>
+            <MarginTop/>
+            <MarginTop/>
+            <ContentFooter>
+                <Tag>
+                    Bug Bounty
+                </Tag>
+                <MinRead>
+                    2 min read
+                </MinRead>
+                <Bookmark>
+                    <MdOutlineBookmarkAdd/>
+                    {/*<MdBookmarkAdded/>*/}
+                    {/*<MdOutlineBookmarkRemove/>*/}
+                    {/*<BsFillBookmarkXFill/>*/}
+                    <AiOutlineShareAlt/>
+                </Bookmark>
+            </ContentFooter>
+
+            <MarginTop/>
+            <MarginTop/>
+
+            <HR/>
+        </WriteUpSection>
     );
 };
 
