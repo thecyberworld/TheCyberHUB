57444ecab6a5fc76a340081d7c4eb51570b1484e
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-08-16 18:46:48 +0530

    Create ResourcesListElements.jsx

---
+++ src/components/Resources/ResourcesListElements.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const ResourcesListSection = styled.div`
+  text-align: center;
+  color: #cecac3;
+  width: 27rem;
+  margin: 200px auto;
+
+  @media screen and (max-width: 760px) {
+    text-align: center;
+  }
+`
+
+export const Heading = styled.h2`
+  margin-top: 35px;
+  text-align: center;
+  color: #cecac3;
+
+  @media screen and (max-width: 760px) {
+    text-align: center;
+  }
+`
+
+export const List = styled.li`
+  color: #cecac3;
+
+`
+
+export const Link = styled.a`
+  color: #cecac3;
+  text-decoration-color: #D1913C;
+  text-decoration-style: dashed;
+
+`
+
+export const ListContent = styled.div`
+  margin-top: 15px;
+  text-align: start;
+  color: #cecac3;
+`
+
+
+
