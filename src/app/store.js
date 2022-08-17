452ca14b1c5634386b7d39b0218dd73687ae2d97
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-08-17 17:27:34 +0530

    Create store.js

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from "@reduxjs/toolkit";
+
+import {cyberNewsApi} from '../services/cyberNewsApi';
+
+export default configureStore({
+    reducer: {
+        [cyberNewsApi.reducerPath]: cyberNewsApi.reducer,
+    },
+})
