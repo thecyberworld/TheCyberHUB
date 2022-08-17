5dc36034efe9a6bcdfe330163cd2f55d728407c9
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-08-17 18:43:21 +0530

    add: Cyber news (#86)
    * Update CyberNews.jsx
    
    * Create Loader.jsx
    
    * Update cyberNewsApi.js

---
+++ src/components/Resources/CyberNews/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Section} from "../WriteUps/WriteUpsElements";
+
+const Loader = () => {
+    return (
+        <Section>
+            <h1>
+                Loading...
+            </h1>
+        </Section>
+    );
+};
+
+export default Loader;
