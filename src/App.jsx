2ece108b7de7b61776da1421e9895ca21eb52bd8
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-08-24 10:04:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,29 +17,28 @@ const App = () => {
 
     const spinnersList = [
         <BarLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <BeatLoader color={"#47cf73"} loading={loading} size={50}/>,
+        <BeatLoader color={"#47cf73"} loading={loading} size={15}/>,
         <BounceLoader color={"#47cf73"} loading={loading} size={60}/>,
-        <CircleLoader color={"#47cf73"} loading={loading} size={150}/>,
-        <ClimbingBoxLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <ClimbingBoxLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <ClipLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <ClockLoader color={"#47cf73"} loading={loading} size={100}/>,
+        <CircleLoader color={"#47cf73"} loading={loading} size={50}/>,
+        <ClimbingBoxLoader color={"#47cf73"} loading={loading} size={15}/>,
+        <ClipLoader color={"#47cf73"} loading={loading} size={35}/>,
+        <ClockLoader color={"#47cf73"} loading={loading} size={50}/>,
         <DotLoader color={"#47cf73"} loading={loading} size={60}/>,
         <FadeLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <GridLoader color={"#47cf73"} loading={loading} size={50}/>,
+        <GridLoader color={"#47cf73"} loading={loading} size={15}/>,
         <HashLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <MoonLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <PacmanLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <PropagateLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <PuffLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <PulseLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <RingLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <RiseLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <RotateLoader color={"#47cf73"} loading={loading} size={50}/>,
+        <MoonLoader color={"#47cf73"} loading={loading} size={60}/>,
+        <PacmanLoader color={"#47cf73"} loading={loading} size={25}/>,
+        <PropagateLoader color={"#47cf73"} loading={loading} size={15}/>,
+        <PuffLoader color={"#47cf73"} loading={loading} size={60}/>,
+        <PulseLoader color={"#47cf73"} loading={loading} size={15}/>,
+        <RingLoader color={"#47cf73"} loading={loading} size={60}/>,
+        <RiseLoader color={"#47cf73"} loading={loading} size={15}/>,
+        <RotateLoader color={"#47cf73"} loading={loading} size={15}/>,
         <ScaleLoader color={"#47cf73"} loading={loading} size={50}/>,
         <SkewLoader color={"#47cf73"} loading={loading} size={50}/>,
         <SquareLoader color={"#47cf73"} loading={loading} size={50}/>,
-        <SyncLoader color={"#47cf73"} loading={loading} size={50}/>,
+        <SyncLoader color={"#47cf73"} loading={loading} size={15}/>,
     ]
 
     let loadingSpinner = spinnersList[Math.floor(
@@ -51,14 +50,14 @@ const App = () => {
         setLoading(true)
         setTimeout(() => {
             setLoading(false)
-        }, 0)
+        }, 2000)
     }, []);
 
     return (
         <div>
             {loading ? (
                 <SpinnerSection>
-                    {/*{loadingSpinner}*/}
+                    {loadingSpinner}
                 </SpinnerSection>
             ) : (
                 <> <ScrollToTop>
