31cc03a210f6faf15201b2df0b6a0ef7a2300f18
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-09-20 17:37:38 +0530

    add: Event (#96)

--- src/components/Resources/Events/Events.jsx
+++ src/components/Resources/Events/Events.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
-import {Section} from "../WriteUps/WriteUpsElements";
+import {
+    EventsContainer,
+    EventHeading,
+    EventContent,
+    ContentList,
+    EventSubHeading,
+    EventSubHeadingSection, BGCTFImage, BGImage, ImageSection
+} from "./EventsElemetns";
+import {Link} from "../RoadmapResources/RoadmapResourcesElements";
+import Video from "../../../assets/videos/video.mp4";
 
 const Events = () => {
     return (
-        <Section>
-            <h1>
-                Under Development
-            </h1>
-        </Section>
+        <EventsContainer>
+            <BGCTFImage>
+                {/*<BGImage src={"/src/assets/ctf.jpg"} type={'image'} />*/}
+            </BGCTFImage>
+            <ImageSection> <img src="/src/assets/Capture.png" alt=""/> </ImageSection>
+            <EventSubHeading> This Saturday @10pm </EventSubHeading>
+            <br/>
+            {/*<EventHeading> TheCyberCTF </EventHeading>*/}
+            <EventSubHeadingSection>
+                <EventSubHeading>
+                    Price: TryHackMe Voucher
+                </EventSubHeading>
+                <EventContent>
+                    Top 3 will get 1 month Voucher.
+                </EventContent>
+            </EventSubHeadingSection>
+            <EventContent>
+                <ContentList>
+                    Join our <Link href="https://discord.gg/QHBPq6xP5p" target={"_blank"}>
+                    <b> Discord </b> </Link> server.
+                </ContentList>
+                <ContentList>For more details and questions.</ContentList>
+                <ContentList>CTF link will be given at discord server before 10m.</ContentList>
+
+                <br/>
+
+                More details will be added soon.
+            </EventContent>
+        </EventsContainer>
     );
 };
 
---
+++ src/components/Resources/Events/EventsElemetns.jsx
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+import CTFImg from "/src/assets/ctf.jpg";
+
+export const EventsContainer = styled.div`
+  text-align: center;
+  margin: 150px 20px;
+  color: #cecac3;
+
+  @media screen and (max-width: 760px) {
+    text-align: center;
+  }
+`
+export const EventHeading = styled.h1`
+  text-align: center;
+  color: #cecac3;
+  margin-bottom: 50px;
+  @media screen and (max-width: 760px) {
+    text-align: center;
+  }
+`
+export const EventSubHeadingSection = styled.div`
+  text-align: center;
+  color: #cecac3;
+  margin-bottom: 50px;
+  @media screen and (max-width: 760px) {
+    text-align: center;
+  }
+`
+export const EventSubHeading = styled.h3`
+  text-align: center;
+  color: #cecac3;
+  margin-bottom: 5px;
+
+  @media screen and (max-width: 760px) {
+    text-align: center;
+  }
+`
+export const ContentList = styled.li`
+  text-align: center;
+  color: #cecac3;
+
+  @media screen and (max-width: 760px) {
+    text-align: center;
+  }
+`
+export const EventContent = styled.p`
+  text-align: center;
+  color: #cecac3;
+
+  @media screen and (max-width: 760px) {
+    text-align: center;
+  }
+`
+
+export const BGImage = styled.img`
+  width: 100%;
+  height: auto;
+  -o-object-fit: cover;
+  object-fit: cover;
+  background: #232a34;
+`
+export const ImageSection = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin: 50px 0;
+  @media screen and (max-width: 760px) {
+    text-align: center;
+  }
+`
+
+export const CtfImage = styled.img`
+
+`
+
+export const BGCTFImage = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`
+
