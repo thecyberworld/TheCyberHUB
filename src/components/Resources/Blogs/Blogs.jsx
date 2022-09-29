3bef0bf0f21a8aa5729e500ee3ae83905a9178ae
aritroCoder <ogcodinggod@gmail.com>
2022-09-29 23:10:19 +0530

    added a blog page

--- src/components/Resources/Blogs/Blogs.jsx
+++ src/components/Resources/Blogs/Blogs.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import {Section} from "../WriteUps/WriteUpsElements";
+import React from "react";
+import { Section } from "../WriteUps/WriteUpsElements";
+import BlogsElements from "./BlogsElements";
+import "./Blog.css";
+
+// probably recieved by a api call
+const blogs = {
+  title: "Lorem, ipsum",
+  author: "Dean A.",
+  date: "1st October 2022",
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex molestias nam, iure hic, sunt totam, eaque corporis inventore mollitia nemo saepe eligendi cumque numquam dolor placeat architecto pariatur fugiat accusantium!\nLorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, cum perspiciatis pariatur facere soluta consectetur rerum aut ex quas deserunt inventore, dignissimos dicta blanditiis? Adipisci incidunt odio eaque nobis earum sit! Sequi, ducimus debitis. Commodi officiis vel quae sequi aspernatur iusto reprehenderit repellendus autem architecto!",
+  tags: ["coding", "gaming"]
+}
 
 const Blogs = () => {
-    return (
-        <Section>
-            <h1> Blogs </h1>
-        </Section>
-    );
+  return (
+    <Section>
+      <h1> All Blogs </h1>
+      <div className="AllBlogs">
+        <BlogsElements title={blogs.title} author={blogs.author} date={blogs.date} content={blogs.content} tags={blogs.tags}/>
+        <BlogsElements title={blogs.title} author={blogs.author} date={blogs.date} content={blogs.content} tags={blogs.tags}/>
+        <BlogsElements title={blogs.title} author={blogs.author} date={blogs.date} content={blogs.content} tags={blogs.tags}/>
+        <BlogsElements title={blogs.title} author={blogs.author} date={blogs.date} content={blogs.content} tags={blogs.tags}/>
+      </div>
+    </Section>
+  );
 };
 
 export default Blogs;
---
+++ src/components/Resources/Blogs/BlogsElements.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Blog.css";
+import image from "./img.jpg";
+// const image = require('./img.jpg');
+
+const BlogsElements = (props) => {
+  return (
+    <>
+      <div className="blogs__container">
+        <div className="blogs__container__title">
+          <img className="blogImg" src={image} alt="Blog Image" width="360px" height="180px" />
+          <h2>{props.title}</h2>
+          <h6>
+            {props.author} | {props.date}
+          </h6>
+        </div>
+        <div className="blogs__container__content">{props.content.slice(0, 200)}{props.content.length>200?"...":""}</div>
+        <div className="tags">
+          tags:
+          {props.tags.map((tag) => (
+            <div className="tag">{tag}</div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BlogsElements;
