83ae4bdf61c1ebf26f4cc84069cbb13463bf2222
abdullmustyy <abdulmusty03@outlook.com>
2022-10-03 12:02:47 +0100

    Add Categories component

--- src/components/Prep/Quiz/Quiz.jsx
+++ src/components/Prep/Quiz/Quiz.jsx
@@ -1,78 +1,88 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import {questions} from './QuizData';
+import Categories from "../../Categories/Categories";
+import { questions } from "./QuizData";
 import {
-    AnswerSection,
-    QuestionButton,
-    QuestionCount,
-    QuestionSection,
-    QuestionText, QuizBody,
-    QuizSection,
-    ResetButton,
-    ScoreInfo,
-    ScoreSection
+  AnswerSection,
+  QuestionButton,
+  QuestionCount,
+  QuestionSection,
+  QuestionText,
+  QuizBody,
+  QuizSection,
+  ResetButton,
+  ScoreInfo,
+  ScoreSection,
 } from "./QuizElements";
 
 const Quiz = () => {
-    const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
-    const [showScore, setShowScore] = useState(false);
+  const [showScore, setShowScore] = useState(false);
 
-    const [score, setScore] = useState(0);
+  const [score, setScore] = useState(0);
 
-    const [scoreList, setScoreList] = useState(0);
+  const [scoreList, setScoreList] = useState(0);
 
-    const handleAnswerButtonClick = (isCorrect) => {
-
-        if (isCorrect === true) {
-            setScore(score + 1);
-
-        }
-
-        const nextQuestion = currentQuestion + 1;
-        if (nextQuestion < questions.length) {
-            setCurrentQuestion(nextQuestion)
-        } else {
-            setShowScore(true)
-        }
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1);
     }
 
-    const handleResetButton = (score) => {
-        setScoreList(scoreList + score);
-        setCurrentQuestion(0);
-        setShowScore(false);
-        setScore(0);
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
     }
+  };
 
+  const handleResetButton = (score) => {
+    setScoreList(scoreList + score);
+    setCurrentQuestion(0);
+    setShowScore(false);
+    setScore(0);
+  };
 
-    return (
-        <QuizSection>
-            {showScore ? (
-                <ScoreSection>
-                    <ScoreInfo>You scored {score} out of {questions.length}</ScoreInfo>
-                    <ResetButton onClick={() => handleResetButton(score)}>
-                        Start again
-                    </ResetButton>
-                </ScoreSection>
-            ) : (
-                <QuizBody>
-                    <QuestionSection>
-                        <QuestionCount>
-                            <span>Question {currentQuestion + 1}</span>/{questions.length}
-                        </QuestionCount>
-                        <QuestionText>{questions[currentQuestion].questionText}</QuestionText>
-                    </QuestionSection>
-                    <AnswerSection>
-                        {questions[currentQuestion].answerOptions.map((answerOption) => (
-                            <QuestionButton onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>
-                                {answerOption.answerText}
-                            </QuestionButton>
-                        ))}
-                    </AnswerSection>
-                </QuizBody>
-            )}
-        </QuizSection>
-    );
+  return (
+    <section>
+      <Categories />
+      <QuizSection>
+        {showScore ? (
+          <ScoreSection>
+            <ScoreInfo>
+              You scored {score} out of {questions.length}
+            </ScoreInfo>
+            <ResetButton onClick={() => handleResetButton(score)}>
+              Start again
+            </ResetButton>
+          </ScoreSection>
+        ) : (
+          <QuizBody>
+            <QuestionSection>
+              <QuestionCount>
+                <span>Question {currentQuestion + 1}</span>/{questions.length}
+              </QuestionCount>
+              <QuestionText>
+                {questions[currentQuestion].questionText}
+              </QuestionText>
+            </QuestionSection>
+            <AnswerSection>
+              {questions[currentQuestion].answerOptions.map((answerOption) => (
+                <QuestionButton
+                  onClick={() =>
+                    handleAnswerButtonClick(answerOption.isCorrect)
+                  }
+                >
+                  {answerOption.answerText}
+                </QuestionButton>
+              ))}
+            </AnswerSection>
+          </QuizBody>
+        )}
+      </QuizSection>
+    </section>
+  );
 };
 
 export default Quiz;
