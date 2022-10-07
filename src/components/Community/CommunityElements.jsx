477fb71612f5baf44f68751956b43ee4335fc7b9
Abdul <87391935+abdullmustyy@users.noreply.github.com>
2022-10-07 20:26:20 +0100

    Add Community Page (#151)
    * Add categories section
    
    * Update packages
    
    * Add Categories component
    
    * Add styled components
    
    * Add functionalities
    
    * Update to fit functionlities
    
    * Add image and video demo
    
    * Apply reset functionality on category change
    
    * fix: quiz design
    
    * fix: conflicts - package-lock.json file
    
    * Initial commit
    
    * Add Community Page section
    
    * Update dependencies
    
    * Update package-lock.json
    
    * Update package.json
    
    * Update Community.jsx
    
    * Update CommunityElements.jsx
    
    Co-authored-by: Kabir <44284877+kabir0x23@users.noreply.github.com>

---
+++ src/components/Community/CommunityElements.jsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import bgImage from "../../assets/thecyberworld-green01.png";
+
+export const Header = styled.header`
+  color: #cecac3;
+  background: url(${bgImage}) no-repeat;
+  background-size: 25%;
+  background-position: 50% 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Overlay = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: #010606;
+  opacity: 0.8;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+
+  & > a {
+    text-decoration: none;
+  }
+`;
+
+export const Heading = styled.h1`
+  color: white;
+  font-size: 3rem;
+  font-weight: 300;
+  margin: 0.5rem 0;
+  text-align: center;
+`;
+
+export const Paragraph = styled.p`
+  font-size: 1.2rem;
+  font-weight: 300;
+  margin: 0.5rem 0 2rem;
+  text-align: center;
+`;
+
+export const Button = styled.button`
+  background-color: #364eb0;
+  border: none;
+  border-radius: 0.3rem;
+  color: white;
+  cursor: pointer;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+
+  & > span {
+    margin: 0 0.2rem;
+  }
+
+  &:hover {
+    background-color: hsl(235, calc(3 * 85.6%), 64.7%);
+  }
+`;
