b90df6c63c1b1a8e6326d3eac15db3278fbefc04
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-10-07 23:28:08 +0530

    Update BlueTeamCoursesData.jsx (#149)

--- src/components/Resources/Courses/YoutubeCoursesData/BlueTeamCoursesData.jsx
+++ src/components/Resources/Courses/YoutubeCoursesData/BlueTeamCoursesData.jsx
@@ -6,7 +6,7 @@ export const BlueTeamCoursesData = [
         YTChannelImageLink: "https://i.ytimg.com/vi/Bt5fh3wQUAQ/maxresdefault.jpg",
         tag: 'Blue Team',
         videoHeading: 'Blue Team Tutorials',
-        videoLink: "https://youtu.be/hDYqWZ11njU?list=PLbyncTkpno5FAC0DJYuJrEqHSMdudEffw",
+        videoLink: "https://www.youtube.com/watch?v=Bt5fh3wQUAQ&list=PLBf0hzazHTGNcIS_dHjM2NgNUFMW1EZFx",
         channelName: "HackerSploit",
         channelLogo: "https://yt3.ggpht.com/ytc/AMLnZu9nSdhYbPUgxIavX6vicAOFioLHhkuCNeHcp-Icpw=s48-c-k-c0x00ffffff-no-rj",
         timeStamp: "Oct 7 2022",
