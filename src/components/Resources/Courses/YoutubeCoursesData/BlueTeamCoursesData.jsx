1273417bb399915377078e27879762a698cbf27a
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-10-07 23:09:08 +0530

    add: courses (#148)

---
+++ src/components/Resources/Courses/YoutubeCoursesData/BlueTeamCoursesData.jsx
@@ -0,0 +1,16 @@
+export const BlueTeamCoursesData = [
+    {
+        id: "freeYoutubeCourses",
+        sectionsHeading: "Blue Team Courses",
+        topLine: 'Free Courses',
+        YTChannelImageLink: "https://i.ytimg.com/vi/Bt5fh3wQUAQ/maxresdefault.jpg",
+        tag: 'Blue Team',
+        videoHeading: 'Blue Team Tutorials',
+        videoLink: "https://youtu.be/hDYqWZ11njU?list=PLbyncTkpno5FAC0DJYuJrEqHSMdudEffw",
+        channelName: "HackerSploit",
+        channelLogo: "https://yt3.ggpht.com/ytc/AMLnZu9nSdhYbPUgxIavX6vicAOFioLHhkuCNeHcp-Icpw=s48-c-k-c0x00ffffff-no-rj",
+        timeStamp: "Oct 7 2022",
+        alt: 'Blue Team Courses',
+        channelLink: "https://www.youtube.com/c/HackerSploit",
+    },
+];
---
+++ src/components/Resources/Courses/YoutubeCoursesData/RedTeamCoursesData.jsx
@@ -0,0 +1,16 @@
+export const RedTeamCoursesData = [
+    {
+        id: "freeYoutubeCourses",
+        sectionsHeading: "Red Team Courses",
+        topLine: 'Free Courses',
+        YTChannelImageLink: "https://i.ytimg.com/vi/EIHLXWnK1Dw/maxresdefault.jpg",
+        tag: 'Red Team',
+        videoHeading: 'Red Team Tutorials',
+        videoLink: "https://youtu.be/hDYqWZ11njU?list=PLbyncTkpno5FAC0DJYuJrEqHSMdudEffw",
+        channelName: "HackerSploit",
+        channelLogo: "https://yt3.ggpht.com/ytc/AMLnZu9nSdhYbPUgxIavX6vicAOFioLHhkuCNeHcp-Icpw=s48-c-k-c0x00ffffff-no-rj",
+        timeStamp: "Oct 7 2022",
+        alt: 'Red Team Courses',
+        channelLink: "https://www.youtube.com/c/HackerSploit",
+    },
+];
