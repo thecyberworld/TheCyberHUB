544810f69c3572f023939bce8767a932e236e343
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-10-07 23:42:09 +0530

    Update RedTeamCoursesData.jsx

--- src/components/Resources/Courses/YoutubeCoursesData/RedTeamCoursesData.jsx
+++ src/components/Resources/Courses/YoutubeCoursesData/RedTeamCoursesData.jsx
@@ -6,7 +6,7 @@ export const RedTeamCoursesData = [
         YTChannelImageLink: "https://i.ytimg.com/vi/EIHLXWnK1Dw/maxresdefault.jpg",
         tag: 'Red Team',
         videoHeading: 'Red Team Tutorials',
-        videoLink: "https://youtu.be/hDYqWZ11njU?list=PLbyncTkpno5FAC0DJYuJrEqHSMdudEffw",
+        videoLink: "https://www.youtube.com/watch?v=EIHLXWnK1Dw&list=PLBf0hzazHTGMjSlPmJ73Cydh9vCqxukCu",
         channelName: "HackerSploit",
         channelLogo: "https://yt3.ggpht.com/ytc/AMLnZu9nSdhYbPUgxIavX6vicAOFioLHhkuCNeHcp-Icpw=s48-c-k-c0x00ffffff-no-rj",
         timeStamp: "Oct 7 2022",
