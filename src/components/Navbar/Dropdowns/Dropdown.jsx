095bfae8f7f2eee02cae0655058b7e8092290563
Abdul <87391935+abdullmustyy@users.noreply.github.com>
2022-10-12 08:19:46 +0100

    Fix Sidebar toggle bug (#171)
    * Add categories section
    
    * Update packages
    
    * Add Categories component
    
    * Add styled components
    
    * Add functionalities
    
    * Update to fit functionlities
    
    * Add image and video demo
    
    * Apply reset functionality on category change
    
    * fix: quiz design
    
    * fix: conflicts - package-lock.json file
    
    * Initial commit
    
    * Add Community Page section
    
    * Update dependencies
    
    * Update package-lock.json
    
    * Update package.json
    
    * Update Community.jsx
    
    * Update CommunityElements.jsx
    
    * Make Quiz Category responsive
    
    * Update dependencies
    
    * Fix mobile quiz dropdown spacing
    
    * Update dependencies
    
    * Update package-lock.json
    
    * Update Categories.jsx
    
    * Update CategoriesButtons.jsx
    
    * Update CategoriesElements.jsx
    
    * Fix Navbar toggle bug
    
    * Update dependencies
    
    * Update dependencies
    
    * Update package-lock.json
    
    Co-authored-by: Kabir <44284877+kabir0x23@users.noreply.github.com>

--- src/components/Navbar/Dropdowns/Dropdown.jsx
+++ src/components/Navbar/Dropdowns/Dropdown.jsx
@@ -1,68 +1,92 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
-import SideDropdown from './SideDropdown';
-import { Link } from 'react-router-dom';
-import {NavLinks, SideDropdownIcon} from '../Navbar/NavbarElements';
+import SideDropdown from "./SideDropdown";
+import { Link } from "react-router-dom";
+import { NavLinks, SideDropdownIcon } from "../Navbar/NavbarElements";
 
 export default function Dropdown(props) {
   const menu = [
-    {title: "Roadmap", url: "/resources/roadmapResources" },
-    {title: "Courses", url: "/resources/courses"},
-    {title: "Prep", url: ""},
-    {title: "Events", url:"/events" },
-    {title: "Blogs", url: "/blogs"},
-    {title: "News", url: "/cyberNews"},
-  ]
+    { title: "Roadmap", url: "/resources/roadmapResources" },
+    { title: "Courses", url: "/resources/courses" },
+    { title: "Prep", url: "" },
+    { title: "Events", url: "/events" },
+    { title: "Blogs", url: "/blogs" },
+    { title: "News", url: "/cyberNews" },
+  ];
 
   const [sideDrop, setSideDrop] = useState(false);
 
-  const sideDropHandler = (title)=> {
-     props.sidebar ?
-     title === 'Prep' && setSideDrop(!sideDrop) :
-     title === 'Prep' ? setSideDrop(true) : setSideDrop(false);
-  }
+  const sideDropHandler = (title) => {
+    props.sidebar
+      ? title === "Prep" && setSideDrop(!sideDrop)
+      : title === "Prep"
+      ? setSideDrop(true)
+      : setSideDrop(false);
+  };
 
   const Item = styled.ul`
-    height: 32px; 
+    height: 32px;
     position: relative;
     &:hover {
-      cursor : pointer;
+      cursor: pointer;
     }
-  `
+  `;
 
-  const mobStyle = {width :'100%', padding: 6};
+  const mobStyle = { width: "100%", padding: 6 };
   const dStyle = {
     borderRadius: 8,
-    background: '#101417',
-    border: '1px solid #101417',
+    background: "#101417",
+    border: "1px solid #101417",
     width: 130,
     padding: 6,
-    color: 'white',
-    position: 'absolute',
-    top: 80
+    color: "white",
+    position: "absolute",
+    top: 80,
   };
 
   return (
     <div style={props.sidebar ? mobStyle : dStyle}>
-        <li>
-            {
-                menu.map(i => (
-                  !props.sidebar ?
-                  <Item onMouseEnter={() => sideDropHandler(i.title)}>
-                    <Link style={{textDecoration: 'none', color:'white' }} to={i.url}> {i.title} </Link >
-                    {i.title === 'Prep' && sideDrop && <SideDropdown sidebar={false}/>}
-                  </Item> :
-                  <>
-                    <Item onClick={() => sideDropHandler(i.title)}>
-                      {i.title !== 'Prep' ?
-                         <Link style={{textDecoration: 'none', color:'white' }} to={i.url}> {i.title} </Link > :
-                         <p style={{textDecoration: 'none', color:'white' }}> {i.title} </p > }
-                    </Item>
-                    {i.title === 'Prep' && sideDrop && <SideDropdown sidebar={true}/>}
-                  </>
-                ))
-            }
-        </li>
+      <li>
+        {menu.map((i) =>
+          !props.sidebar ? (
+            <Item onMouseEnter={() => sideDropHandler(i.title)}>
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to={i.url}
+              >
+                {" "}
+                {i.title}{" "}
+              </Link>
+              {i.title === "Prep" && sideDrop && (
+                <SideDropdown sidebar={false} />
+              )}
+            </Item>
+          ) : (
+            <>
+              <Item onClick={() => sideDropHandler(i.title)}>
+                {i.title !== "Prep" ? (
+                  <Link
+                    onClick={props.toggle}
+                    style={{ textDecoration: "none", color: "white" }}
+                    to={i.url}
+                  >
+                    {" "}
+                    {i.title}{" "}
+                  </Link>
+                ) : (
+                  <p style={{ textDecoration: "none", color: "white" }}>
+                    {" "}
+                    {i.title}{" "}
+                  </p>
+                )}
+              </Item>
+              {i.title === "Prep" && sideDrop && (
+                <SideDropdown toggle={props.toggle} sidebar={true} />
+              )}
+            </>
+          )
+        )}
+      </li>
     </div>
-  )
+  );
 }
