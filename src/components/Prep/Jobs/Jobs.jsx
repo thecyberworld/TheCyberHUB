251c7c72fdb6d5cbaecf6322160d74028022dbaf
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-10-15 15:30:43 +0530

    fix: job

--- src/components/Prep/Jobs/Jobs.jsx
+++ src/components/Prep/Jobs/Jobs.jsx
@@ -1,81 +1,13 @@
-import React from "react";
-import {
-  JobSection,
-  JobContainer,
-  JobHero,
-  JobHeader,
-  JobTitle,
-  JobRole,
-  JobRoleTitle,
-  JobRating,
-  JobType,
-  JobDetails,
-  JobState,
-} from "./JobsElements";
-
-import { FaEllipsisV } from "react-icons/fa";
+import React from 'react';
+import {Section} from "../../Resources/WriteUps/WriteUpsElements";
 
 const Jobs = () => {
   return (
-    <JobSection>
-      <div className="grid">
-        <JobContainer>
-          <JobHero>
-            <JobHeader>
-              <span>new</span>
-              <JobTitle className="text-8xl">Data Analysts</JobTitle>
-              <JobRole>
-                <JobRoleTitle>Cloudstaff</JobRoleTitle>
-                <JobRating>4.1⭐</JobRating>
-              </JobRole>
-              <JobType>
-                <span>Remote</span>
-              </JobType>
-            </JobHeader>
-            <FaEllipsisV />
-          </JobHero>
-          <JobDetails>
-            <div>
-              <span>💼</span>
-              <span>Full-time</span>
-            </div>
-            <div>
-              <span>⏱️</span>
-              <span>Morning shifts</span>
-            </div>
-          </JobDetails>
-          <JobState>
-            <div>
-              <span>⏩</span>
-              <span>Apply securely with Indeed Resume</span>
-            </div>
-            <div>
-              <span>⚡</span>
-              <span>Responsive employer</span>
-            </div>
-          </JobState>
-          <div className="job-details">
-            <ul>
-              <li>
-                Technical expertise in data models, database design development,
-                data mining, and segmentation technique.
-              </li>
-              <li>
-                Strong knowledge of and experience with reporting packages
-                (Business Objects, etc.), databases...
-              </li>
-            </ul>
-          </div>
-          <div className="job-timeline">
-            <span>Posted 3 days ago</span>
-            <span></span>
-            <span>
-              From <span>remote</span>
-            </span>
-          </div>
-        </JobContainer>
-      </div>
-    </JobSection>
+      <Section>
+        <h1>
+          Under Development
+        </h1>
+      </Section>
   );
 };
 
