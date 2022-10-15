d2e804aa89f1b91e58eb1c80039e6641d0917ecc
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-10-15 15:31:06 +0530

    Update Jobs.jsx

--- src/components/Prep/Jobs/Jobs.jsx
+++ src/components/Prep/Jobs/Jobs.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {Section} from "../../Resources/WriteUps/WriteUpsElements";
 
 const Jobs = () => {
-  return (
-      <Section>
-        <h1>
-          Under Development
-        </h1>
-      </Section>
-  );
+    return (
+        <Section>
+            <h1>
+                Under Development
+            </h1>
+        </Section>
+    );
 };
 
 export default Jobs;
