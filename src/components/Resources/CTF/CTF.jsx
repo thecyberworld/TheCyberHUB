580acddfa287f7bdbde9fcf680b9177485057604
Tanya Arora <tanyaa.ar28@gmail.com>
2022-10-17 05:09:18 +0530

    Apply custom CSS to Submit page

--- src/components/Resources/CTF/CTF.jsx
+++ src/components/Resources/CTF/CTF.jsx
@@ -27,11 +27,11 @@ const CTF = () => {
             <br />
             <div className="AllEvents">
                 {/* <CTFElements venue={CtfsData.venue} date={CtfsData.date} content={CtfsData.content}/> */}
-                <div className="flex flex-col m-4">
-                    <label htmlFor="value" className="text-2xl font-semibold">Enter value:</label>
-                    <input type="text" id="value" className="text-black m-4 px-[8px] h-8 w-52 rounded-sm font-semibold"
+                <div className="submit-container">
+                    <label htmlFor="value" className="label-css">Enter value:</label>
+                    <input type="text" id="value" className="input-css"
                         value={value} onChange={e => setValue(e.target.value)}></input>
-                    {correct? <button className="bg-green-600 border border-white text-lg w-28 h-[40px] rounded-md font-semibold self-center m-4 hover:scale-110" onClick={handleClick}>Submit</button>:<button className="bg-black border-solid border border-white text-green-500 text-lg w-28 h-[40px] rounded-md font-semibold self-center m-4 hover:scale-110" onClick={handleClick}>Submit</button>}
+                    {correct? <button className="button-final" onClick={handleClick}>Submit</button>:<button className="button-initial" onClick={handleClick}>Submit</button>}
                     {correct?<p>Congratulations!</p>:null}
                 </div>
             </div>
