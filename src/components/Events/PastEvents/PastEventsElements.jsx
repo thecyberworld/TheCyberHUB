16bd005bc5d5cae32a4d54536386101a769c2bcc
Kabir <97401889+KingD3v@users.noreply.github.com>
2022-10-17 22:56:45 +0530

    fix: css margin

--- src/components/Events/PastEvents/PastEventsElements.jsx
+++ src/components/Events/PastEvents/PastEventsElements.jsx
@@ -10,7 +10,7 @@ export const PastEventsContainer = styled.div`
   border: 1px solid #ffffff0d;
   border-radius: 10px;
   background-color: #0d1117;
-  margin: 0 20px 0 ;
+  margin: 0 0 20px 0 ;
   padding: 15px 15px;
   @media screen and (max-width: 768px) {
     width: 100%;
