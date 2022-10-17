04538125ea3e73704aea474c260da8f20444483d
Elliot Alderson <116019210+E11i0tald3rs0n@users.noreply.github.com>
2022-10-17 22:43:34 +0530

    Update ComingSoon.jsx (#213)

--- src/components/MixComponents/ComingSoon.jsx
+++ src/components/MixComponents/ComingSoon.jsx
@@ -4,7 +4,7 @@ import '../../index.css';
 const ComingSoon = () => {
     return (
         <div style={{background:'#010606FF', color: '#fff', textAlign: "center", padding: "0 0 200px 0", margin: "0 0 0 0"}}>
-            <h1>👨‍💻 <i>More on the way</i> 👩‍💻  </h1>
+//             <h1>👨‍💻 <i>More on the way</i> 👩‍💻  </h1>
         </div>
     );
 };
