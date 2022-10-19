d91067b35a17f5f1b1010f604f0690f0b2f93441
Aksa12 <41188167+Aksa12@users.noreply.github.com>
2022-10-19 15:02:34 +0500

    fix: Alignment issue on the events page (#224)
    Co-authored-by: aksa <aksa@jobcase.com>

--- src/components/Events/OnGoingEvents/OnGoingEventsElements.jsx
+++ src/components/Events/OnGoingEvents/OnGoingEventsElements.jsx
@@ -10,8 +10,12 @@ export const OnGoingEventsContainer = styled.div`
   border: 1px solid #ffffff0d;
   border-radius: 10px;
   background-color: #0d1117;
-  margin: 20px 20px;
+  margin: 0 20px 20px 0;
   padding: 15px 20px;
+
+  @media screen and (max-width: 768px) {
+    margin: 0 0 20px 0;
+  }
 `
 
 export const OnGoingEvents_Image = styled.img`
