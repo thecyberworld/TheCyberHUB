87e457a5c46fd1f4be24bb1a7a5e656983d8ef82
abdullmustyy <abdulmusty03@outlook.com>
2022-10-20 16:50:38 +0100

    Add chatbot

---
+++ src/components/Chatbot/Widgets/BotComponents/TCHBotAvatar.jsx
@@ -0,0 +1,14 @@
+import BotAvatar from "../../../../assets/thecyberworld-green01.png";
+
+export default function TCHBotAvatar() {
+  return (
+    <div className="react-chatbot-kit-chat-bot-avatar">
+      <div
+        className="react-chatbot-kit-chat-bot-avatar-container"
+        style={{ background: "none" }}
+      >
+        <img alt="BotAvatar" src={BotAvatar} />
+      </div>
+    </div>
+  );
+}
