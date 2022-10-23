c061a1e2d567e0eafe5972a3debf425f1a8053df
Niraj Pradhan <59108522+nirajpdn@users.noreply.github.com>
2022-10-24 01:08:56 +0545

    fix: footer sticks at bottom with flex & margin (#257)
    Co-authored-by: Kabir <44284877+kabir0x23@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -36,76 +36,81 @@ import OpenSourceProjects from "./components/OpenSourceProjects/OpenSourceProjec
 
 import Spinner from "./components/MixComponents/Spinner/Spinner";
 import Submit from "./components/Resources/Submit/Submit";
-import Certificate from "./components/Certificate/Certificate";
+import Layout from "./components/Layout";
 
 const App = () => {
-    const [loading, setLoading] = useState(false);
-    const {pathname} = useLocation();
+  const [loading, setLoading] = useState(false);
+  const { pathname } = useLocation();
 
-    useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 5000);
-    }, []);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
 
-    const showDefaultLayout = () => {
-        return pathname !== "/register";
-    };
+  const showFooter = () => {
+    return pathname !== "/register";
+  };
 
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-    return (
-        <div>
-            {loading ? (<Spinner/>) :
-                (<>
-                        {/* Required on registration page. Do not remove. */}
-                        {showDefaultLayout() && <navbar>
-                            <Sidebar isOpen={isOpen} toggle={toggle}/>
-                            <Navbar toggle={toggle}/>
-                        </navbar>}
+  return (
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Layout>
+          <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
+          </>
 
-                        <ScrollToTop>
-                            <Routes>
-                                <Route index exact path={"/"} element={<Homepage/>}/>
+          <ScrollToTop>
+            <Routes>
+              <Route index exact path={"/"} element={<Homepage />} />
 
-                                <Route exact path={"/blogs/*"} element={<Blogs/>}/>
-                                <Route exact path={"/events"} element={<Events/>}/>
-                                <Route exact path={"/community"} element={<Community/>}/>
-                                <Route exact path={"/about"} element={<About/>}/>
-                                <Route exact path={"/projects/*"} element={<OpenSourceProjects/>}/>
-                                <Route exact path={"/CyberGames"} element={<CyberGames/>}/>
-                                <Route exact path={"/CyberGames/CTF"} element={<CTF/>}/>
-                                <Route exact path={"/CyberGames/OSINTGame"} element={<OSINTGame/>}/>
-                                <Route exact path={"/submit"} element={<Submit/>}/>
+              <Route exact path={"/blogs/*"} element={<Blogs />} />
+              <Route exact path={"/events"} element={<Events />} />
+              <Route exact path={"/community"} element={<Community />} />
+              <Route exact path={"/about"} element={<About />} />
+              <Route exact path={"/projects/*"} element={<OpenSourceProjects />} />
+              <Route exact path={"/CyberGames"} element={<CyberGames />} />
+              <Route exact path={"/CyberGames/CTF"} element={<CTF />} />
+              <Route exact path={"/CyberGames/OSINTGame"} element={<OSINTGame />} />
+              <Route exact path={"/submit"} element={<Submit />} />
 
+              <Route exact path={"/resources/*"} element={<Resources />}>
+                <Route index element={<ResourcesHome />} />
+                <Route path={"roadmapResources"} element={<RoadmapResources />} />
+                <Route path={"cyberNews"} element={<CyberNews />} />
+                <Route path={"blogs"} element={<Blogs />} />
+                <Route path={"events"} element={<Events />} />
 
-                                <Route exact path={"/resources/*"} element={<Resources/>}>
-                                    <Route index element={<ResourcesHome/>}/>
-                                    <Route path={"roadmapResources"} element={<RoadmapResources/>}/>
-                                    <Route path={"courses"} element={<Courses/>}/>
-                                    <Route path={"cyberNews"} element={<CyberNews/>}/>
-                                    <Route path={"blogs"} element={<Blogs/>}/>
-                                    <Route path={"events"} element={<Events/>}/>
-                                </Route>
+                <Route path="courses" element={<CoursesLayout />}>
+                  <Route index element={<Courses />} />
+                  <Route path=":id" element={<CourseDetail />} />
+                </Route>
+              </Route>
 
-                                <Route exact path={"/prep"} element={<Prep/>}>
-                                    <Route path={"interviewQuestions"} element={<InterviewQuestions/>}/>
-                                    <Route path={"interviewExperiences"} element={<InterviewExperiences/>}/>
-                                    <Route path={"quiz"} element={<Quiz/>}/>
-                                    <Route path={"jobs"} element={<Jobs/>}/>
-                                </Route>
+              <Route exact path={"/prep"} element={<Prep />}>
+                <Route path={"interviewQuestions"} element={<InterviewQuestions />} />
+                <Route path={"interviewExperiences"} element={<InterviewExperiences />} />
+                <Route path={"quiz"} element={<Quiz />} />
+                <Route path={"jobs"} element={<Jobs />} />
+              </Route>
 
-                                <Route exact path={"/register"} element={<Registration/>}></Route>
-                            </Routes>
-                        </ScrollToTop>
-                        {/* Required on registration page. Do not remove. */}
-                        {showDefaultLayout() && <Footer/>}
-                    </>
-                )}
-        </div>
-    );
+              <Route exact path={"/register"} element={<Registration />}></Route>
+            </Routes>
+          </ScrollToTop>
+          {/*{showFooter() &&*/}
+              <Footer />
+          {/*}*/}
+        </Layout>
+      )}
+    </div>
+  );
 };
 
 export default App;
