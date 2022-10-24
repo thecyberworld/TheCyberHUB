e499b23b2e0e8ef03103e23c3915f1af32da9091
Anuran Das <104062485+Anuran12@users.noreply.github.com>
2022-10-24 10:38:27 +0530

    add: image behind the container (#251)
    * Added image behind the container
    
    * fix: packagefiles
    
    Co-authored-by: Kabir <44284877+kabir0x23@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 8080
+  },
   plugins: [react()],
 });
