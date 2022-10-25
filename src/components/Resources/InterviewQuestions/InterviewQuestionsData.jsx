6b7a46923f41cc4da71d42f9476e2b4384dd53a9
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-10-25 17:11:23 +0530

    feat: interview questions page and new layout (#267)

---
+++ src/components/Resources/InterviewQuestions/InterviewQuestionsData.jsx
@@ -0,0 +1,325 @@
+const InterviewsQuestionsData = [
+    {
+        id: 1,
+        title: 'Encryption and Authentication',
+        level: 'Beginner',
+        desc: 'Step by step guide to learn Cyber Security',
+        details: [
+            {
+                questions: 'How do cookies work?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'How do sessions work?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'Explain how OAuth works?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'What is a public key infrastructure flow and how would I diagram it?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'Describe the difference between synchronous and asynchronous encryption?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'Describe SSL handshake?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'How does HMAC work?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'Why HMAC is designed in that way?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'What is the difference between authentication vs authorization name spaces?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'What’s the difference between Diffie-Hellman and RSA?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'How does Kerberos work?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'If you\'re going to compress and encrypt a file, which do you do first and why??',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'How do I authenticate you and know you sent the message?',
+                answer: 'What is a three-way handshake?',
+            }, {
+                questions: 'Should you encrypt all data at rest?',
+                answer: 'What is a three-way handshake?',
+            },
+            {
+                questions: 'What is Perfect Forward Secrecy?',
+                answer: 'What is a three-way handshake?',
+            },
+        ],
+    }, {
+        id: 1,
+        title: 'Network Level and Logging',
+        level: 'Beginner',
+        desc: 'Step by step guide to learn Cyber Security',
+        details: [
+            {
+                questions: 'What are common ports involving security, what are the risks and mitigations?',
+                answer: '',
+            },
+            {
+                questions: 'Which one for DNS?',
+                answer: '',
+            },
+            {
+                questions: 'Describe HTTPs and how it is used?',
+                answer: '',
+            },
+            {
+                questions: 'What is the difference between HTTPS and SSL?',
+                answer: '',
+            },
+            {
+                questions: 'How does threat modeling work?',
+                answer: '',
+            },
+            {
+                questions: 'What is a subnet and how is it useful in security?',
+                answer: '',
+            },
+            {
+                questions: 'What is subnet mask?',
+                answer: '',
+            },
+            {
+                questions: 'Explain what traceroute is?',
+                answer: '',
+            },
+            {
+                questions: 'Draw a network, then expect them to raise an issue and have to figure out where it happened?',
+                answer: '',
+            },
+            {
+                questions: 'Write out a Cisco ASA firewall configuration on the white board to allow three networks unfiltered access, 12 networks limited access to different resources on different networks, and 8 networks to be blocked altogether.?',
+                answer: '',
+            },
+            {
+                questions: 'Explain TCP/IP concepts?',
+                answer: '',
+            },
+            {
+                questions: 'What is OSI model?',
+                answer: '',
+            },
+            {
+                questions: 'How does a router differ from a switch?',
+                answer: '',
+            },
+            {
+                questions: 'Describe the Risk Management Framework process and a project where you successfully implemented compliance with RMF.?',
+                answer: '',
+            },
+            {
+                questions: 'How does a packet travel between two hosts connected in same network?',
+                answer: '',
+            },
+            {
+                questions: 'Explain the difference between TCP and UDP?',
+                answer: '',
+            },
+            {
+                questions: 'Which is more secure and why?',
+                answer: '',
+            },
+            {
+                questions: 'What is the TCP three way handshake?',
+                answer: '',
+            },
+            {
+                questions: 'What is the difference between IPSEC Phase 1 and Phase 2?',
+                answer: '',
+            },
+            {
+                questions: 'What are biggest AWS security vulnerabilities?',
+                answer: '',
+            },
+            {
+                questions: 'How do web certificates for HTTPS work?',
+                answer: '',
+            },
+            {
+                questions: 'What is the purpose of TLS?',
+                answer: '',
+            },
+            {
+                questions: 'Is ARP UDP or TCP?',
+                answer: '',
+            },
+
+        ]
+    }, {
+        id: 1,
+        title: 'OWASP Top 10, Pentesting and/or Web Applications',
+        level: 'Beginner',
+        desc: 'Step by step guide to learn Cyber Security',
+        details: [
+            {
+                questions: 'Differentiate XSS from CSRF.',
+                answer: '',
+            }, {
+                questions: 'What do you do if a user brings you a pc that is acting weird? You suspect malware.',
+                answer: '',
+            }, {
+                questions: 'What is the difference between tcp dump and FWmonitor?',
+                answer: '',
+            }, {
+                questions: 'Do you know what XXE is?',
+                answer: '',
+            }, {
+                questions: 'Explain man-in-the-middle attacks.',
+                answer: '',
+            }, {
+                questions: 'What is a Server Side Request Forgery attack?',
+                answer: '',
+            }, {
+                questions: 'Describe what are egghunters and their use in exploit development.',
+                answer: '',
+            }, {
+                questions: 'How is pad lock icon in browser generated?',
+                answer: '',
+            }, {
+                questions: 'What is Same Origin Policy and CORS?',
+                answer: '',
+            }
+        ]
+    }, {
+        id: 1,
+        title: 'Databases',
+        level: 'Beginner',
+        desc: 'Step by step guide to learn Cyber Security',
+        details: [
+            {
+                questions: 'How would you secure a Mongo database?',
+                answer: '',
+            }, {
+                questions: 'Postgres?',
+                answer: '',
+            }, {
+                questions: 'Our DB was stolen/exfiltrated. It was secured with one round of sha256 with a static salt?, What do we do now?, Are we at risk?, What do we change?',
+                answer: '',
+            }, {
+                questions: 'What are the 6 aggregate functions of SQL?',
+                answer: '',
+            },
+        ]
+
+    }, {
+        id: 1,
+        title: 'Tools and Games',
+        level: 'Beginner',
+        desc: 'Step by step guide to learn Cyber Security',
+        details: [
+            {
+                questions: 'Have I played CTF?',
+                answer: '',
+            }, {
+                questions: 'Would you decrypt a steganography image?',
+                answer: '',
+            }, {
+                questions: 'You\'re given an ip-based phone and asked me to decrypt the message in the phone.',
+                answer: '',
+            }, {
+                questions: 'What CND tools do you knowledge or experience with?',
+                answer: '',
+            }, {
+                questions: 'What is the difference between nmap -ss and nmap -st?',
+                answer: '',
+            }, {
+                questions: 'How would you filter xyz in Wireshark?',
+                answer: '',
+            }, {
+                questions: 'Given a sample packet capture - Identify the protocol, the traffic, and the likelihood of malicious intent. ',
+                answer: '',
+            }, {
+                questions: 'If left alone in office with access to a computer, how would you exploit it?',
+                answer: '',
+            }, {
+                questions: 'How do you fingerprint an iPhone so you can monitor it even after wiping it?',
+                answer: '',
+            }, {
+                questions: 'How would you use CI/CD to improve security?',
+                answer: '',
+            }, {
+                questions: 'You have a pipeline for Docker images. How would you design everything to ensure the proper security checks? ',
+                answer: '',
+            }, {
+                questions: 'How would you create a secret storage system?',
+                answer: '',
+            }, {
+                questions: 'What technical skill or project are you working on for fun in your free time?',
+                answer: '',
+            }, {
+                questions: 'How would you harden your work laptop if you needed it at Defcon?',
+                answer: '',
+            }, {
+                questions: 'If you had to set up supply chain attack prevention, how would you do that?',
+                answer: '',
+            },
+        ]
+    },
+    {
+        id: 1,
+        title: 'Programming and Code',
+        level: 'Beginner',
+        desc: 'Step by step guide to learn Cyber Security',
+        details: [
+            {
+                questions: 'Code review a project and look for the vulnerability.',
+                answer: '',
+            }, {
+                questions: 'How would you conduct a security code review?',
+                answer: '',
+            }, {
+                questions: 'How can Github webhooks be used in a malicious way?',
+                answer: '',
+            }, {
+                questions: 'Given a CVE, walk us through it and how the solution works.',
+                answer: '',
+            }, {
+                questions: 'Tell me about a repetitive task at work that you automated away.',
+                answer: '',
+            },
+        ]
+    },
+    {
+        id: 1,
+        title: 'Compliance',
+        level: 'Beginner',
+        desc: 'Step by step guide to learn Cyber Security',
+        details: [
+            {
+            questions: 'Can you explain SOC 2?',
+            answer: '',
+        },         {
+            questions: 'What are the five trust criteria?',
+            answer: '',
+        },         {
+            questions: 'How is ISO27001 different?',
+            answer: '',
+        },         {
+            questions: 'Can you list examples of controls these frameworks require?',
+            answer: '',
+        },         {
+            questions: 'What is the difference between Governance, Risk and Compliance?',
+            answer: '',
+        },         {
+            questions: 'What does Zero Trust mean?',
+            answer: '',
+        },         {
+            questions: 'What is role-based access control (RBAC) and why is it covered by compliance frameworks?',
+            answer: '',
+        },         {
+            questions: 'What is the NIST framework and why is it influential?',
+            answer: '',
+        },
+        ]
+    }
+]
+
+export default InterviewsQuestionsData
--- src/components/Resources/InterviewQuestions/InterviewQuestionsElements.jsx
+++ src/components/Resources/InterviewQuestions/InterviewQuestionsElements.jsx
@@ -1,21 +1,46 @@
 import styled from "styled-components";
 
 export const InterviewQuestionSection = styled.div`
+  max-width: 80em;
   text-align: center;
   color: #cecac3;
-  width: 27rem;
   margin: 150px auto;
 
   @media screen and (max-width: 600px) {
     width: 20rem;
   }
 `;
-export const ListContent = styled.div`
-  margin-top: 15px;
+
+
+export const InterviewsQuestionsCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
   text-align: start;
-  color: #cecac3;
+  width: 45em;
+  color: white;
+  margin: 15px 0;
+  background: #0a0a0a;
+  border: #383838 1px solid;
+  border-radius: 3px;
+  padding: 5px 15px;
+  transition: all 0.3s ease-in-out;
 `;
 
-export const List = styled.li`
-  margin: 5px;
+export const InterviewsQuestionsHeading = styled.h2`
+  margin: 10px 5px;
+  min-width: 20px;
+  color: #91dc56;
+  font-size: 18px;
+  width: 100%;
+
+`;
+export const InterviewsQuestionsTitle = styled.div`
+  //border: #4cc23e 1px solid;
+  border-radius: 3px;
+  display: flex;
+  margin: 0 0 5px 0;
+  padding: 5px;
+  cursor: pointer;
+
 `;
