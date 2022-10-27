c6f4aa02110e464f5294de178a7306b23253f104
Sameer Kumar <kumar.sam1612@gmail.com>
2022-10-27 12:32:20 +0530

    refactor [#183]: Migrated blog to styled components.

--- src/components/Learn/Blogs/ViewBlog.jsx
+++ src/components/Learn/Blogs/ViewBlog.jsx
@@ -1,32 +1,56 @@
 import React from "react";
-import { useSearchParams, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import { Section } from "../../Resources/WriteUps/WriteUpsElements";
 import image from "./img.webp";
-import "./Blog.css";
 
 import blogs from "./BlogsData";
 import { encodeURL } from "./util";
+import { Tag, Tags } from "./BlogCard";
+
+const ViewBlogComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 50px 300px;
+  padding-top: 0;
+  align-items: center;
+  justify-content: space-between;
+  font-family: "Montserrat", sans-serif;
+
+  * {
+    margin: 10px 0;
+  }
+
+  p {
+    text-align: left;
+    padding: 0 50px;
+    white-space: pre-line;
+    font-size: 20px;
+  }
+
+  img {
+    margin-top: 0;
+  }
+`;
 
 const ViewBlog = () => {
   const { title } = useParams();
   let searchedBlog = blogs.find((blog) => encodeURL(blog.title).toLowerCase() === title.toLowerCase());
   return (
     <Section>
-      <div className="viewBlog">
+      <ViewBlogComponent>
         <img className="viewImg" src={image} alt="Blog Image" />
         <h1>{searchedBlog.title}</h1>
         <h3>
           {searchedBlog.author} | {searchedBlog.date}
         </h3>
         <p>{searchedBlog.content}</p>
-      </div>
-      <div className="tags">
+      </ViewBlogComponent>
+      <Tags>
         {searchedBlog.tags.map((tag, index) => (
-          <div className="tag" key={index}>
-            {tag}
-          </div>
+          <Tag key={index}>{tag}</Tag>
         ))}
-      </div>
+      </Tags>
     </Section>
   );
 };
