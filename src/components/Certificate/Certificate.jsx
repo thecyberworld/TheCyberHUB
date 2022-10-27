5d1b71e3a9cd5ba44fce589e2ebf5b00f4d20ea6
Sameer Kumar <kumar.sam1612@gmail.com>
2022-10-27 12:55:28 +0530

    migrated certificates to styled components

--- src/components/Certificate/Certificate.jsx
+++ src/components/Certificate/Certificate.jsx
@@ -1,64 +1,62 @@
 import { useEffect, useState } from "react";
-import "./certificate.scss";
 import axios from "axios";
-import { CertificateContainer } from "./CertificateElements";
+import { CertificateContainer, CertificateComponent, CertificateFooter } from "./CertificateElements";
 
 export default function Certificate(certId) {
-    const [certificate, setCertificate] = useState();
-    const baseUrl = "https://thecyberhub-next.vercel.app/api/";
+  const [certificate, setCertificate] = useState();
+  const baseUrl = "https://thecyberhub-next.vercel.app/api/";
 
-    useEffect(() => {
-        async function fetchCertificate() {
-            const res = await axios.get(baseUrl + "get-certificate?id=" + certId.certId);
-            const data = await res.data;
-            setCertificate(data);
-        }
+  useEffect(() => {
+    async function fetchCertificate() {
+      const res = await axios.get(baseUrl + "get-certificate?id=" + certId.certId);
+      const data = await res.data;
+      setCertificate(data);
+    }
 
-        fetchCertificate();
-    }, []);
+    fetchCertificate();
+  }, []);
 
-    return (
-        <CertificateContainer>
-            {certificate ? (
-                <div className="certificate flex-column-center">
-                    <div className="certificate-word">Certificate</div>
-                    <div className="achievement-word">of achievement</div>
-                    <div className="presented-word">This certificate is presented to</div>
+  return (
+    <CertificateContainer>
+      {certificate ? (
+        <CertificateComponent>
+          <div className="certificate-word">Certificate</div>
+          <div className="achievement-word">of achievement</div>
+          <div className="presented-word">This certificate is presented to</div>
+          <div className="full-name">{certificate.fullName}</div>
+          <div className="ctf">
+            for {getKindText(certificate.kind)} - {certificate.ctf}
+          </div>
+          <div className="description">{certificate.description}</div>
 
-                    <div className="full-name">{certificate.fullName}</div>
-                    <div className="ctf">
-                        for {getKindText(certificate.kind)} - {certificate.ctf}
-                    </div>
-                    <div className="description">{certificate.description}</div>
-
-                    <div className="footer">
-                        <div className="date-block">
-                            <div className="date">{certificate.issueDate}</div>
-                            <div className="date-word">Date</div>
-                        </div>
-                        <div className="certificate-id flex-column-center">
-                            <p>Id: {certificate.id}</p>
-                        </div>
-                        <div className="signature-block">
-                            <div className="signature flex-column-center">Some image here</div>
-                            <div className="signature-word flex-column-center">signature</div>
-                        </div>
-                    </div>
-                </div>
-            ) : (
-                ""
-            )}
-        </CertificateContainer>
-    );
+          <CertificateFooter>
+            <div className="date-block">
+              <div className="date">{certificate.issueDate}</div>
+              <div className="date-word">Date</div>
+            </div>
+            <div className="certificate-id">
+              <p>Id: {certificate.id}</p>
+            </div>
+            <div className="signature-block">
+              <div className="signature">Some image here</div>
+              <div className="signature-word">signature</div>
+            </div>
+          </CertificateFooter>
+        </CertificateComponent>
+      ) : (
+        ""
+      )}
+    </CertificateContainer>
+  );
 }
 
 function getKindText(kind) {
-    switch (kind) {
-        case "winner":
-            return "winning";
-        case "participation":
-            return "participating in";
-        default:
-            return "";
-    }
+  switch (kind) {
+    case "winner":
+      return "winning";
+    case "participation":
+      return "participating in";
+    default:
+      return "";
+  }
 }
--- src/components/Certificate/CertificateElements.jsx
+++ src/components/Certificate/CertificateElements.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import bgImage from "./../../assets/images/certificate-bg.png";
 
 export const CertificateContainer = styled.div`
   display: flex;
@@ -6,4 +7,91 @@ export const CertificateContainer = styled.div`
   align-items: center;
   margin: 50px auto;
   color: #fff;
-`
+`;
+
+export const CertificateComponent = styled.div`
+  width: 60em;
+  padding: 1rem 5rem;
+  font-family: "Oswald", sans-serif;
+  text-transform: uppercase;
+  color: whitesmoke;
+  background-image: url(${bgImage});
+  background-repeat: no-repeat;
+  background-size: cover;
+  border: 0.3rem solid #222;
+  align-items: center;
+  border-radius: 0.6rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .certificate-word {
+    margin-top: 4rem;
+    font-weight: 500;
+    font-size: 4rem;
+  }
+
+  .achievement-word {
+    margin-top: 1rem;
+    font-weight: 400;
+    font-size: 2rem;
+  }
+
+  .presented-word {
+    margin-top: 1.5rem;
+    font-weight: 300;
+    font-size: 1.5rem;
+  }
+
+  .full-name {
+    padding: 1rem 2rem 0.5rem 2rem;
+    margin-top: 3rem;
+    font-weight: 300;
+    font-size: 2rem;
+    border-bottom: 2px solid #ccc;
+  }
+
+  .description {
+    margin-top: 3rem;
+    max-width: 50vw;
+  }
+`;
+
+export const CertificateFooter = styled.div`
+  min-width: 60em;
+  max-width: 60em;
+  padding: 0px 50px;
+  margin-top: 4rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 50vw;
+
+  .date-block {
+    .date {
+      padding: 1rem 1rem 0.5rem 1rem;
+      border-bottom: 2px solid #ccc;
+    }
+
+    .date-word {
+      text-align: center;
+      font-size: 1.5rem;
+    }
+  }
+
+  .certificate-id {
+    justify-content: end;
+  }
+
+  .signature-block {
+    .signature {
+      padding: 1rem 1rem 0.5rem 1rem;
+      border-bottom: 2px solid #ccc;
+    }
+
+    .signature-word {
+      text-align: center;
+      font-size: 1.5rem;
+    }
+  }
+`;
