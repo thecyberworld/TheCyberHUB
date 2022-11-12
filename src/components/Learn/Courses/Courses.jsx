142ad5b3f8510dc28564cd100af255ab8611a43e
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-11-12 13:48:08 +0530

    courses

--- src/components/Learn/Courses/Courses.jsx
+++ src/components/Learn/Courses/Courses.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useParams } from "react-router";
 
+import "./VideoCard/VideoCard.css";
 import {
     LinuxYoutubeVideosCard,
     CyberYoutubeVideosCard,
@@ -9,40 +9,40 @@ import {
     BlueTeamCoursesCard,
 } from "./VideoCard/VideoCard";
 
-const COURSES_CARD_INFO = {
-    linux: {
+const COURSES_CARD_INFO = [
+    {
         title: "Linux",
         content: LinuxYoutubeVideosCard,
     },
-    cyber_security: {
+    {
         title: "Cyber Security",
         content: CyberYoutubeVideosCard,
     },
-    bug_hunting: {
+    {
         title: "Bug bounty hunting",
         content: BugHuntingVideosCard,
     },
-    red_team: {
+    {
         title: "Red Team",
         content: RedTeamCoursesCard,
     },
-    blue_team: {
+    {
         title: "Blue Team",
         content: BlueTeamCoursesCard,
     },
-};
+];
 
-const YoutubeCourseDetail = () => {
-    const params = useParams();
-
+const FreeYoutubeCourses = () => {
     return (
         <>
-            <div className={"card__section"} id={"id"}>
-                <h1 className={"h1_Courses"}>{COURSES_CARD_INFO[params.id].title}</h1>
-                <div className="container">{COURSES_CARD_INFO[params.id].content}</div>
-            </div>
+            {COURSES_CARD_INFO.map((props) => (
+                <div className={"card__section"} key={props.title}>
+                    <h1 className={"h1_Courses"}>{props.title}</h1>
+                    <div className="container">{props.content}</div>
+                </div>
+            ))}
         </>
     );
 };
 
-export default YoutubeCourseDetail;
+export default FreeYoutubeCourses;
