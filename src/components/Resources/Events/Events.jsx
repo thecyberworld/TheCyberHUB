d24adb51382a0e24cab42bc028430f4b03c712a3
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-11-13 23:01:54 +0530

    fix: CTF data

--- src/components/Resources/Events/Events.jsx
+++ src/components/Resources/Events/Events.jsx
@@ -28,81 +28,86 @@ const Events = () => {
         (a, b) => new Date(a.validationDate) - new Date(b.validationDate),
     );
 
+    const pastEvents = allEvents.filter((event) => new Date(event.validationDate) < new Date()).reverse();
+    const tenPastEvents = pastEvents.slice(0, 8);
+
     const todayDate = new Date(Date());
 
     return (
         <EventsContainer>
             <MainEventsContainer>
-                {allEvents.some((event) => formatDate(todayDate) === formatDate(new Date(event.validationDate))) && (
+                {allEvents.some((event) => formatDate(todayDate) === formatDate(new Date(event.validationDate))) ? (
                     <>
                         <EventsHeading>Today Events</EventsHeading>
                         <OnGoingEventsContainer>
-                            {allEvents.map(
-                                (event, id) =>
-                                    formatDate(todayDate) === formatDate(new Date(event.validationDate)) && (
-                                        <RouterLink key={id} to={{ pathname: `${encodeURL(event.title)}` }}>
-                                            <OnGoingEvents
-                                                key={id}
-                                                title={event.title}
-                                                image={event.image}
-                                                venue={event.venue}
-                                                location={event.location}
-                                                url={event.url}
-                                                date={event.date}
-                                                content={event.content}
-                                            />
-                                        </RouterLink>
-                                    ),
+                            {allEvents.map((event, id) =>
+                                formatDate(todayDate) === formatDate(new Date(event.validationDate)) ? (
+                                    <RouterLink key={id} to={{ pathname: `${encodeURL(event.title)}` }}>
+                                        <OnGoingEvents
+                                            key={id}
+                                            title={event.title}
+                                            image={event.image}
+                                            venue={event.venue}
+                                            location={event.location}
+                                            url={event.url}
+                                            date={event.date}
+                                            content={event.content}
+                                        />
+                                    </RouterLink>
+                                ) : null,
                             )}
                         </OnGoingEventsContainer>
                     </>
-                )}
+                ) : null}
 
-                {allEvents.some((event) => formatDate(todayDate) < formatDate(new Date(event.validationDate))) && (
+                {allEvents.some((event) => formatDate(todayDate) < formatDate(new Date(event.validationDate))) ? (
                     <>
                         <EventsHeading>Up Coming Events</EventsHeading>
                         <UpComingEventsContainer>
-                            {allEvents.map((event, id) => (
-                                <RouterLink key={id} to={{ pathname: `${encodeURL(event.title)}` }}>
-                                    <UpComingEvents
-                                        key={id}
-                                        title={event.title}
-                                        image={event.image}
-                                        venue={event.venue}
-                                        location={event.location}
-                                        url={event.url}
-                                        date={event.date}
-                                        content={event.content}
-                                    />
-                                </RouterLink>
-                            ))}
+                            {allEvents.map((event, id) =>
+                                formatDate(todayDate) < formatDate(new Date(event.validationDate)) ? (
+                                    <RouterLink key={id} to={{ pathname: `${encodeURL(event.title)}` }}>
+                                        <UpComingEvents
+                                            key={id}
+                                            title={event.title}
+                                            image={event.image}
+                                            venue={event.venue}
+                                            location={event.location}
+                                            url={event.url}
+                                            date={event.date}
+                                            content={event.content}
+                                        />
+                                    </RouterLink>
+                                ) : null,
+                            )}
                         </UpComingEventsContainer>
+                    </>
+                ) : (
+                    <>
+                        <EventsHeading>No Up Coming Events</EventsHeading>
                     </>
                 )}
             </MainEventsContainer>
 
-            {allEvents.some((event) => formatDate(todayDate) > formatDate(new Date(event.validationDate))) && (
-                <PastEventsContainer>
-                    <EventsHeading>Past Events</EventsHeading>
-                    {allEvents.map(
-                        (event, id) =>
-                            formatDate(todayDate) > formatDate(new Date(event.validationDate)) && (
-                                <RouterLink key={id} to={{ pathname: `${encodeURL(event.title)}` }}>
-                                    <PastEvents
-                                        key={id}
-                                        title={event.title}
-                                        image={event.image}
-                                        venue={event.venue}
-                                        location={event.location}
-                                        url={event.url}
-                                        date={event.date}
-                                        content={event.content}
-                                    />
-                                </RouterLink>
-                            ),
-                    )}
-                </PastEventsContainer>
-            )}
+            <PastEventsContainer>
+                <EventsHeading>Past Events</EventsHeading>
+                {tenPastEvents.map((event, id) =>
+                    formatDate(todayDate) > formatDate(new Date(event.validationDate)) ? (
+                        <RouterLink key={id} to={{ pathname: `${encodeURL(event.title)}` }}>
+                            <PastEvents
+                                key={id}
+                                title={event.title}
+                                image={event.image}
+                                venue={event.venue}
+                                location={event.location}
+                                url={event.url}
+                                date={event.date}
+                                content={event.content}
+                            />
+                        </RouterLink>
+                    ) : null,
+                )}
+            </PastEventsContainer>
         </EventsContainer>
     );
 };
