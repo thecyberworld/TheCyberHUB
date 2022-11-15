3168e1594d12e06680629106d83ef910f0b4f09b
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-11-15 17:47:46 +0530

    fix: sponsors page issues (#304)
    * add: sponsors
    
    * fix: sponsors height
    
    * fix: sponsors page issues

--- src/components/Sponsors/SponsorsElements.jsx
+++ src/components/Sponsors/SponsorsElements.jsx
@@ -3,23 +3,25 @@ import styled from "styled-components";
 export const SponsorsContainer = styled.div`
     padding: 10px 20px;
     width: 100%;
-    margin: auto;
     display: flex;
     flex-direction: column;
     justify-content: center;
     color: #fff;
-    margin-top: 150px;
-`;
+    margin: 150px auto;
 
-export const BuyMeACoffeeImage = styled.img`
-    margin: 50px auto;
+    @media screen and (max-width: 768px) {
+        margin: 100px auto;
+    }
 `;
 
 export const SponsorsH1 = styled.h1`
     font-family: "Roboto Mono", monospace;
-
     text-align: center;
     margin: 0 auto 50px auto;
+
+    @media screen and (max-width: 768px) {
+        font-size: 1.8rem;
+    }
 `;
 
 export const SponsorsP = styled.p`
@@ -29,6 +31,7 @@ export const SponsorsP = styled.p`
 `;
 
 export const SponsorsLink = styled.a`
+    border: #4cc23e 1px solid;
     font-family: "Roboto Mono", monospace;
     display: flex;
     justify-content: center;
@@ -40,9 +43,24 @@ export const SponsorsLink = styled.a`
     font-size: 1rem;
     font-weight: 600;
     text-align: center;
+    cursor: none;
 
     &:hover {
         color: #c7c7c7;
         text-decoration: none;
+    }
+`;
+
+export const BuyMeACoffeeImage = styled.img`
+    margin: 50px auto;
+    cursor: pointer;
+
+    &:hover {
+        transform: scale(1.1);
+        transition: all 0.3s ease-in-out;
+    }
+
+    @media screen and (max-width: 768px) {
+        margin: 0;
     }
 `;
