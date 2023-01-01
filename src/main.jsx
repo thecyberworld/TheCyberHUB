0b8ea3ba91110c2fbb28cd771b59f5d37e1e1f06
Kabir <44284877+kabir0x23@users.noreply.github.com>
2023-01-01 19:27:44 +0530

    proxy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {
+    // BrowserRouter,
+    HashRouter,
+} from "react-router-dom";
 import { Provider } from "react-redux";
 // import { hydrate, render } from "react-dom";
 
@@ -13,11 +16,13 @@ const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
     <>
         <React.StrictMode>
-            <BrowserRouter>
+            {/* <BrowserRouter> */}
+            <HashRouter>
                 <Provider store={store}>
                     <App />
                 </Provider>
-            </BrowserRouter>
+            </HashRouter>
+            {/* </BrowserRouter> */}
         </React.StrictMode>
     </>,
 );
