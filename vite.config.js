1fa890a2af88484cdefdc0b604cf5a7c47b57f07
Kabir <44284877+kabir0x23@users.noreply.github.com>
2023-02-08 01:31:11 +0530

    add: image handles, blog features

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-let targetURL = "http://localhost:5000";
-if (import.meta.env.VITE_WEB_ENV === "production") {
-    targetURL = import.meta.env.VITE_API_URL;
-} else targetURL = "http://localhost:5000";
-
+// https://vitejs.dev/config/
 export default defineConfig({
     server: {
         proxy: {
+            // "/api": "http://localhost:5000",
+            // "/api": "https://api.thecyberhub.org",
             "/api": {
-                target: targetURL,
+                target: import.meta.env.VITE_API_URL,
+                // target: "http://localhost:5000",
+                // target: "https://api.thecyberhub.org",
+                // target: "https://dev.api.thecyberhub.org",
                 changeOrigin: true,
                 secure: false,
             },
@@ -18,5 +19,8 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 3000,
     },
+    // preview: {
+    //     port: 8080
+    // },
     plugins: [react()],
 });
