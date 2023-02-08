d30881ff310cb933b23fd06d2e85fd8a8c5d344b
Kabir <44284877+kabir0x23@users.noreply.github.com>
2023-02-08 21:49:26 +0530

    fix: user blogs

--- src/components/Dashboard/Blogs/UserBlogs.jsx
+++ src/components/Dashboard/Blogs/UserBlogs.jsx
@@ -1,54 +1,9 @@
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getBlogs, reset } from "../../../features/blogs/blogSlice";
-import { Wrapper } from "../Profile/ProfileElements";
-import NewBlogCard from "../../Blogs/BlogCard/NewBlogCard";
-import { CircleSpinner } from "react-spinners-kit";
+import React from "react";
+
+import Blogs from "../../Blogs/Blogs";
 
 const UserBlogs = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const { user } = useSelector((state) => state.auth);
-    const { blogs, isLoading, isError, message } = useSelector((state) => state.blogs);
-
-    useEffect(() => {
-        if (isError) {
-            console.log(message);
-        }
-        if (!user) navigate("/login");
-        else dispatch(getBlogs());
-
-        return () => dispatch(reset());
-    }, [user, navigate, dispatch, isError, message]);
-
-    if (isLoading) <CircleSpinner size={20} color={"#1fc10d"} />;
-
-    return (
-        <Wrapper>
-            <Link to={"create"} style={{ color: "cornflowerblue" }}>
-                <h2>Create a new blog</h2>
-            </Link>
-            {blogs && blogs?.length > 0 ? (
-                <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
-                    {blogs.map((blog) => (
-                        <NewBlogCard key={blog?._id} blog={blog} />
-                    ))}
-                </div>
-            ) : (
-                <div
-                    style={{
-                        fontFamily: "Roboto Mono, monospace",
-                        color: "#ababab",
-                        textAlign: "center",
-                        margin: "100px 0 0 0",
-                    }}
-                >
-                    <h3 style={{ fontSize: "30px" }}> You have not set any blogs</h3>
-                </div>
-            )}
-        </Wrapper>
-    );
+    return <Blogs />;
 };
 
 export default UserBlogs;
