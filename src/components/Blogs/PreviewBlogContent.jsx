a86d61fd50b5212c2cb9967631636f8cd4451524
Kabir <44284877+kabir0x23@users.noreply.github.com>
2023-03-03 21:37:35 +0530

    changes

--- src/components/Blogs/PreviewBlogContent.jsx
+++ src/components/Blogs/PreviewBlogContent.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { ContentSection, PreviewMarkdownHeading, Tag, TagsSection } from "./ViewBlog/ViewBlogElements";
 import PreviewMarkdown from "./ViewBlog/PreviewMarkdown";
+import { useLocation } from "react-router-dom";
 
 const PreviewBlogContent = (props) => {
+    const { pathname } = useLocation();
     return (
         <div>
-            <PreviewMarkdownHeading>{props.title !== "" ? props.title : "PLease add a Heading"}</PreviewMarkdownHeading>
+            <PreviewMarkdownHeading>{props.title !== "" ? props.title : "Please add a Heading"}</PreviewMarkdownHeading>
             <ContentSection>
                 <PreviewMarkdown content={props.content} />
             </ContentSection>
             <TagsSection>
-                {props.tags.length !== 0 && props.tags[0] !== ""
-                    ? props.tags.split(", ").map((tag, id) => <Tag key={id}>{tag}</Tag>)
+                {pathname === "/dashboard/blogs/create"
+                    ? props.tags.length !== 0 && props.tags[0] !== ""
+                        ? props.tags.split(/,\s*/).map((tag, id) => <Tag key={id}>{tag}</Tag>)
+                        : "Please add some Tags"
+                    : props?.tags?.length !== 0 && props?.tags[0] !== ""
+                    ? props.tags.map((tag, id) => <Tag key={id}>{tag}</Tag>)
                     : "Please add some Tags"}
             </TagsSection>
         </div>
