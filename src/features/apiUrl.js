10276880afa737ed7011af255b928a8edb9dc0cd
Kabir <44284877+kabir0x23@users.noreply.github.com>
2023-04-14 22:59:14 +0530

    Update apiUrl.js

--- src/features/apiUrl.js
+++ src/features/apiUrl.js
@@ -2,7 +2,8 @@
 // const cdnUrl = import.meta.env.VITE_CDN_URL;
 // const webEnv = import.meta.env.VITE_WEB_ENV;
 // const apiUrl = "https://dev.api.thecyberhub.org";
-const apiUrl = "https://api.thecyberhub.org";
+// const apiUrl = "https://api.thecyberhub.org";
+const apiUrl = "https://api-thecyberhub-org.azurewebsites.net";
 // const cdnUrl = "https://thecyberhub.nyc3.cdn.digitaloceanspaces.com";
 const cdnUrl = "https://thecyberhubstorage.blob.core.windows.net";
 const localUrl = "http://localhost:5000";
