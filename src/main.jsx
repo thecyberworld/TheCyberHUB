f2ffe8971c0f0e173db38ce2d87378ab84ccb263
Kabir <44284877+kabir0x23@users.noreply.github.com>
2023-04-27 18:31:12 +0530

    fix routers

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
@@ -9,14 +9,22 @@ import store from "./app/store";
 
 const rootElement = document.getElementById("root");
 
+export const webEnv = import.meta.env.VITE_WEB_ENV || "production";
+
 ReactDOM.createRoot(rootElement).render(
-    <>
-        <React.StrictMode>
+    <React.StrictMode>
+        {webEnv === "production" ? (
             <BrowserRouter>
                 <Provider store={store}>
                     <App />
                 </Provider>
             </BrowserRouter>
-        </React.StrictMode>
-    </>,
+        ) : (
+            <HashRouter>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </HashRouter>
+        )}
+    </React.StrictMode>,
 );
