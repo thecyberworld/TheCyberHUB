4d7d554fb906cd012761d12e5b1942f55850839b
Tandin Tshewang <tandintshewang123@gmail.com>
2023-10-14 02:55:15 +0600

    feat: feature timeline events

--- src/components/Opportunities/TimeLineEvent/TimeLineEvent.jsx
+++ src/components/Opportunities/TimeLineEvent/TimeLineEvent.jsx
@@ -1,35 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
     ParentContainer,
     Container,
     Header,
     SubHeader,
     Tabs,
-    BookingList,
-    BookingItem,
     AiFillClockCircleIcon,
     MdLocationOnIcon,
-    BiChevronDownIcon,
     BiSendIcon,
     BiUserPlusIcon,
     AiOutlineCloseCircleIcon,
-    BiSolidChevronUpIcon,
-    AiFillExclamationCircleIcon
+    NoDataComponent
 } from "./TimeLineEventsElement";
 import eventsData from '../../../events.json';
+import NoDatafound from "../../../assets/images/no_data_found.svg";
+import { BookingItemList } from "./BookingListItem";
 
 const TimeLineEvent = () => {
-    const [isOpenBooking, setIsOpenBooking] = useState(false);
-    const [isActiveTab, setActiveTab] = useState(0);
     const events = eventsData.events;
-
-    const actions = [
-        { icon: AiFillClockCircleIcon, text: 'Reschedule booking' },
-        { icon: BiSendIcon, text: 'Request reschedule' },
-        { icon: MdLocationOnIcon, text: 'Edit location' },
-        { icon: BiUserPlusIcon, text: 'Invite people' },
-        { icon: AiOutlineCloseCircleIcon, text: 'Cancel event' }
-    ];
+    const [openBookingIndex, setOpenBookingIndex] = useState(null);
+    const [isActiveTab, setActiveTab] = useState(0);
+    const today = new Date();
+    const todayString = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
 
     const tabNames = [
         { id: 0, status: 'upcoming' },
@@ -38,11 +30,17 @@ const TimeLineEvent = () => {
         { id: 3, status: 'past' },
         { id: 4, status: 'cancelled' }];
 
-    const today = new Date();
-    const todayString = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
-    const filteredEvents = events.filter(event => event.status === tabNames[isActiveTab].status);
+    const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 
-    console.log(filteredEvents, 'filteredEvents');
+    const actionsIcon = [
+        { icon: AiFillClockCircleIcon, text: 'Reschedule booking' },
+        { icon: BiSendIcon, text: 'Request reschedule' },
+        { icon: MdLocationOnIcon, text: 'Edit location' },
+        { icon: BiUserPlusIcon, text: 'Invite people' },
+        { icon: AiOutlineCloseCircleIcon, text: 'Cancel event' }
+    ];
+
+    const filteredEvents = events.filter(event => event.status === tabNames[isActiveTab].status);
 
     return (
         <ParentContainer>
@@ -60,75 +58,31 @@ const TimeLineEvent = () => {
                         </button>
                     ))}
                 </Tabs>
-                <BookingList>
+                <div className="booking-list">
                     {
-                        events.map((data, index) => {
+
+                        filteredEvents.length !== 0 ? filteredEvents.map((data, index) => {
                             const dateObject = new Date(data.date);
-                            const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
                             const dayName = daysOfWeek[dateObject.getDay()];
-                            const [isRequestedEvent, setRequestedEvent] = useState(false);
 
-                            useEffect(() => {
-                                setRequestedEvent(data.reschedule);
-                            }, [data.reschedule]);
+                            return (
+                                <BookingItemList
+                                    data={data}
+                                    todayString={todayString}
+                                    dayName={dayName}
+                                    actions={actionsIcon}
+                                    openBookingIndex={openBookingIndex}
+                                    setOpenBookingIndex={setOpenBookingIndex}
+                                    key={index}
+                                    index={index} />
+                            )
+                        }) :
+                            <NoDataComponent>
+                                <h2>There is no {tabNames[isActiveTab].status} event right now.</h2>
+                                <img src={NoDatafound} alt="Np data found" />
+                            </NoDataComponent>}
 
-                            return (<BookingItem isRequestedEvent={isRequestedEvent} key={index}>
-                                <div className={data.date === todayString ? "date today-date" : "date"}>
-                                    <p>{dayName}</p>
-                                    <p className="date-digit">{data.date.split("-")[2]}</p>
-                                </div>
-                                <div className="time-line">
-                                    <div className="time-line-detail">
-                                        <AiFillClockCircleIcon />
-                                        <p>{data.startTime} - {data.endTime}</p>
-                                        {isRequestedEvent &&
-                                            <div className="time-line-request">
-                                                <AiFillExclamationCircleIcon />
-                                            </div>
-                                        }
-                                    </div>
-
-                                    <div className="time-line-detail">
-                                        <MdLocationOnIcon />
-                                        <p className="text-over-flow">{data.location}</p>
-                                    </div>
-                                </div>
-                                <div className="details">
-                                    <div>
-                                        <p>{data.eventName}</p>
-                                        <div className="details-profile">
-                                            {data.participants.map((participant, pIndex) => (
-                                                <img src={participant.profileURL} alt={participant.name} key={pIndex} />
-                                            ))}
-                                        </div>
-                                    </div>
-
-                                    {isRequestedEvent &&
-                                        <div className="details-request">
-                                            15:30 - 16:00 requested
-                                        </div>}
-                                </div>
-                                <div className="action">
-                                    <div onClick={() => setIsOpenBooking(!isOpenBooking)} className="action-edit">
-                                        <p>Edit</p>
-                                        {isOpenBooking ? <BiSolidChevronUpIcon /> : <BiChevronDownIcon />}
-                                    </div>
-
-                                    {isOpenBooking && (
-                                        <div className="action-dropdown">
-                                            {actions.map(({ icon: Icon, text }) => (
-                                                <div className="action-dropdown-list" onClick={() => setIsOpenBooking(!isOpenBooking)} key={text}>
-                                                    <Icon />
-                                                    <p>{text}</p>
-                                                </div>
-                                            ))}
-                                        </div>
-                                    )}
-                                </div>
-                            </BookingItem>)
-                        })}
-
-                </BookingList>
+                </div>
             </Container>
         </ParentContainer>
     );
