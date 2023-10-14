985844aae510e18cf457433906a2d0a0c780f4ae
Kabir <44284877+kabir0x23@users.noreply.github.com>
2023-10-14 20:02:24 +0530

    add: event description

--- src/components/Opportunities/TimeLineEvent/TimeLineEvent.jsx
+++ src/components/Opportunities/TimeLineEvent/TimeLineEvent.jsx
@@ -10,10 +10,10 @@ import {
     BiSendIcon,
     BiUserPlusIcon,
     AiOutlineCloseCircleIcon,
-    NoDataComponent
+    NoDataComponent,
 } from "./TimeLineEventsElement";
-import eventsData from '../../../events.json';
-import NoDatafound from "../../../assets/images/no_data_found.svg";
+import eventsData from "./events.json";
+import NoDataFound from "../../../assets/images/no_data_found.svg";
 import { BookingItemList } from "./BookingListItem";
 
 const TimeLineEvent = () => {
@@ -21,26 +21,29 @@ const TimeLineEvent = () => {
     const [openBookingIndex, setOpenBookingIndex] = useState(null);
     const [isActiveTab, setActiveTab] = useState(0);
     const today = new Date();
-    const todayString = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+    const todayString = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, "0")}-${String(
+        today.getDate(),
+    ).padStart(2, "0")}`;
 
     const tabNames = [
-        { id: 0, status: 'upcoming' },
-        { id: 1, status: 'pending' },
-        { id: 2, status: 'recurring' },
-        { id: 3, status: 'past' },
-        { id: 4, status: 'cancelled' }];
-
-    const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-
-    const actionsIcon = [
-        { icon: AiFillClockCircleIcon, text: 'Reschedule booking' },
-        { icon: BiSendIcon, text: 'Request reschedule' },
-        { icon: MdLocationOnIcon, text: 'Edit location' },
-        { icon: BiUserPlusIcon, text: 'Invite people' },
-        { icon: AiOutlineCloseCircleIcon, text: 'Cancel event' }
+        { id: 0, status: "upcoming" },
+        { id: 1, status: "pending" },
+        { id: 2, status: "recurring" },
+        { id: 3, status: "past" },
+        { id: 4, status: "cancelled" },
     ];
 
-    const filteredEvents = events.filter(event => event.status === tabNames[isActiveTab].status);
+    const daysOfWeek = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
+
+    const actionsIcon = [
+        { icon: AiFillClockCircleIcon, text: "Reschedule booking" },
+        { icon: BiSendIcon, text: "Request reschedule" },
+        { icon: MdLocationOnIcon, text: "Edit location" },
+        { icon: BiUserPlusIcon, text: "Invite people" },
+        { icon: AiOutlineCloseCircleIcon, text: "Cancel event" },
+    ];
+
+    const filteredEvents = events.filter((event) => event.status === tabNames[isActiveTab].status);
 
     return (
         <ParentContainer>
@@ -52,16 +55,15 @@ const TimeLineEvent = () => {
                         <button
                             onClick={() => setActiveTab(index)}
                             key={tab.id}
-                            className={isActiveTab === index ? 'active' : ''}
+                            className={isActiveTab === index ? "active" : ""}
                         >
                             {tab.status}
                         </button>
                     ))}
                 </Tabs>
                 <div className="booking-list">
-                    {
-
-                        filteredEvents.length !== 0 ? filteredEvents.map((data, index) => {
+                    {filteredEvents.length !== 0 ? (
+                        filteredEvents.map((data, index) => {
                             const dateObject = new Date(data.date);
                             const dayName = daysOfWeek[dateObject.getDay()];
 
@@ -74,19 +76,20 @@ const TimeLineEvent = () => {
                                     openBookingIndex={openBookingIndex}
                                     setOpenBookingIndex={setOpenBookingIndex}
                                     key={index}
-                                    index={index} />
-                            )
-                        }) :
-                            <NoDataComponent>
-                                <h2>There is no {tabNames[isActiveTab].status} event right now.</h2>
-                                <img src={NoDatafound} alt="Np data found" />
-                            </NoDataComponent>}
-
+                                    index={index}
+                                />
+                            );
+                        })
+                    ) : (
+                        <NoDataComponent>
+                            <h2>There is no {tabNames[isActiveTab].status} event right now.</h2>
+                            <img src={NoDataFound} alt="Np data found" />
+                        </NoDataComponent>
+                    )}
                 </div>
             </Container>
         </ParentContainer>
     );
 };
-
 
 export default TimeLineEvent;
--- src/components/Opportunities/TimeLineEvent/TimeLineEventsElement.jsx
+++ src/components/Opportunities/TimeLineEvent/TimeLineEventsElement.jsx
@@ -11,71 +11,71 @@ export const ParentContainer = styled.div`
 `;
 
 export const Container = styled.div`
-  width: 80%;
-  margin: 3rem auto;
+    width: 80%;
+    margin: 3rem auto;
 `;
 
 export const Header = styled.h1`
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
-  font-weight: 800;
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+    font-weight: 800;
 `;
 
 export const SubHeader = styled.p`
-  font-size: 1rem;
-  margin-bottom: 2.5rem;
-  color: gray;
+    font-size: 1rem;
+    margin-bottom: 2.5rem;
+    color: gray;
 `;
 
 export const Tabs = styled.div`
-  display: flex;
-  gap: 1rem;
-  margin-bottom: 2.5rem;
-  background-color: #FFFFFF;
-  width: fit-content;
-  border-radius: 8px;
-  padding: 0.5rem;
-
-  button {
-    padding: 0.75rem 2.5rem;
-    border: none;
-    background-color: transparent;
-    cursor: pointer;
+    display: flex;
+    gap: 1rem;
+    margin-bottom: 2.5rem;
+    background-color: #ffffff;
+    width: fit-content;
     border-radius: 8px;
-    font-size: 1rem;
-    font-weight: 500;
-    text-transform: capitalize;
+    padding: 0.5rem;
 
-    &:hover {
-      background-color: #DDD;
-    }
+    button {
+        padding: 0.75rem 2.5rem;
+        border: none;
+        background-color: transparent;
+        cursor: pointer;
+        border-radius: 8px;
+        font-size: 1rem;
+        font-weight: 500;
+        text-transform: capitalize;
 
-    &.active {
-      background-color: #DDD;
+        &:hover {
+            background-color: #ddd;
+        }
+
+        &.active {
+            background-color: #ddd;
+        }
     }
-  }
 `;
 
 export const NoDataComponent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: fit-content;
-  
-  img {
-    margin-top: 2rem;
-    width: 300px;
-    height: 300px;
-  }
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: fit-content;
+
+    img {
+        margin-top: 2rem;
+        width: 300px;
+        height: 300px;
+    }
 `;
 
 const createStyledIcon = (IconComponent) => {
-  return styled(IconComponent)`
+    return styled(IconComponent)`
         width: 20px;
         height: 20px;
     `;
-}
+};
 
 export const AiFillClockCircleIcon = createStyledIcon(AiFillClockCircle);
 export const MdLocationOnIcon = createStyledIcon(MdLocationOn);
