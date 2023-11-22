1abf7ba3f41ae520d617f6d14fedf3f79dc388db
Wamir <wamir@localhost.localdomain>
2023-11-23 00:06:56 +0530

    feat: FeedTags

--- src/components/Feeds/Feeds.jsx
+++ src/components/Feeds/Feeds.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState,useEffect } from "react";
 import { FeedsContainer, MiddleSection } from "./FeedsElements";
 import { Wrapper } from "../Dashboard/Profile/ProfileElements";
 import AddFeed from "./PostForm/AddFeed";
@@ -9,6 +9,13 @@ import { getAllUserDetails, userDetailReset } from "../../features/userDetail/us
 import LoadingSpinner from "../Other/MixComponents/Spinner/LoadingSpinner";
 import UnderMaintenance from "../Other/UnderMaintenance/UnderMaintenance";
 import apiStatus from "../../features/apiStatus";
+import FeedTags from "./FeedTags/FeedTags";
+import {
+    LeftContainer,
+    SearchContainer,
+} from "../Explore/ExploreElements";
+import { SearchBox, SearchIcon, SearchInput } from "../CaptureTheFlag/CTFElements";
+
 
 const Feeds = () => {
     const dispatch = useDispatch();
@@ -17,7 +24,7 @@ const Feeds = () => {
     const { userDetails, isUserDetailLoading, isUserDetailError, userDetailMessage } = useSelector(
         (state) => state.userDetail,
     );
-
+    
     useEffect(() => {
         if (isFeedError) console.log(feedMessage);
         if (isUserDetailError) console.log(userDetailMessage);
@@ -30,6 +37,12 @@ const Feeds = () => {
             dispatch(userDetailReset());
         };
     }, [dispatch]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+    const feedTags = feeds?.map((feed) => feed && feed?.tags).flat() || [];
 
     const combinedData = feeds?.map((feed) => {
         const userDetail = userDetails?.find((user) => user?.user === feed?.user);
@@ -50,6 +63,20 @@ const Feeds = () => {
                     <AddFeed showPostTags={true} userDetails={userDetails} />
                     <FeedPosts feeds={combinedData} isFeedLoading={isFeedLoading} />
                 </MiddleSection>
+                <LeftContainer style={{padding:"25px 0"}}>
+                    <SearchContainer>
+                        <SearchBox>
+                            <SearchIcon />
+                            <SearchInput
+                                type="text"
+                                placeholder="Search by name"
+                                value={searchTerm}
+                                onChange={handleSearchTermChange}
+                            />
+                        </SearchBox>
+                    </SearchContainer>
+                    <FeedTags tags={feedTags} />
+                </LeftContainer>
             </FeedsContainer>
         </Wrapper>
     );
