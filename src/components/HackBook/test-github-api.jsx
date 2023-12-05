4d5f21dba3acf09713713a880813848c70e50fa9
Kabir <44284877+kabir0x23@users.noreply.github.com>
2023-12-05 15:06:42 +0530

    feat: Hackbook | base - 2

--- src/components/HackBook/test-github-api.jsx
+++ src/components/HackBook/test-github-api.jsx
@@ -1,27 +1,93 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Wrapper } from "../Dashboard/Profile/ProfileElements";
+import styled from "styled-components";
+import PreviewMarkdown from "../Blogs/ViewBlog/PreviewMarkdown";
 
-const MarkdownContentFromGitHub = ({ owner, repo, path }) => {
-    const [markdownContent, setMarkdownContent] = useState("");
+const LeftSection = styled.div`
+    min-width: 500px;
+    max-width: 500px;
+    width: 100%;
+    flex: 1;
+    padding: 20px;
+`;
+
+const RightSection = styled.div`
+    flex: 1;
+    width: 100%;
+    padding: 20px;
+`;
+
+const MarkdownContentFromGitHub = () => {
+    const [contents, setContents] = useState([]);
+    const [selectedFile, setSelectedFile] = useState(null);
 
     useEffect(() => {
-        const url = `https://api.github.com/users/thecyberworld/repos/thecyberhub`;
-        axios
-            .get(url)
-            .then((response) => response.data)
-            .then((data) => {
-                if (data.type === "file") {
-                    fetch(data.download_url)
-                        .then((response) => response.text())
-                        .then((text) => setMarkdownContent(text));
-                }
-            });
-    }, [owner, repo, path]);
+        const apiUrl = "https://api.github.com/repos/thecyberworld/hacklist/contents/";
+
+        const fetchContents = async (url) => {
+            try {
+                const response = await axios.get(url);
+                const filteredContents = response.data.filter(
+                    (item) =>
+                        (item.type === "dir" && item.name !== "images") ||
+                        (item.type === "file" && item.name.endsWith(".md") && item.name !== "README.md"),
+                );
+
+                setContents(filteredContents);
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        };
+
+        fetchContents(apiUrl);
+    }, []);
+
+    const handleFileClick = async (file) => {
+        try {
+            if (file.type === "file") {
+                const response = await axios.get(file.download_url);
+                setSelectedFile({ ...file, content: response.data });
+            } else if (file.type === "dir") {
+                const response = await axios.get(file.url);
+                const nestedContents = response.data.filter(
+                    (item) =>
+                        (item.type === "dir" && item.name !== "images") ||
+                        (item.type === "file" && item.name.endsWith(".md") && item.name !== "README.md"),
+                );
+                setContents(nestedContents);
+                setSelectedFile(null);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    const handleItemClick = (item) => {
+        handleFileClick(item);
+    };
 
     return (
         <Wrapper>
-            {markdownContent ? <div dangerouslySetInnerHTML={{ __html: markdownContent }} /> : <p>Loading...</p>}
+            <div style={{ display: "flex" }}>
+                <LeftSection>
+                    <ul>
+                        {contents.map((item, id) => (
+                            <li key={id} onClick={() => handleItemClick(item)}>
+                                {item.name}
+                            </li>
+                        ))}
+                    </ul>
+                </LeftSection>
+                <RightSection>
+                    {selectedFile && (
+                        <div>
+                            <h3>{selectedFile.name}</h3>
+                            <PreviewMarkdown content={selectedFile.content} />
+                        </div>
+                    )}
+                </RightSection>
+            </div>
         </Wrapper>
     );
 };
