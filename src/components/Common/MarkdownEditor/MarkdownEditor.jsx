2bb638087c5be69c5094a329e9da2033ffea0a0b
Arkadi Koifman <76536506+ArkadiK94@users.noreply.github.com>
2023-12-10 11:37:56 +0200

    feat: notes | make checkboxes clickable (#536)
    * [Feat] Connect Notes To Backend
    
    why: so the notes will be stored in db
    how: change the redux slice file, add fetching data with axios
    
    * [Feat] Connect Notes To Backend
    
    why: so the notes will be stored in db
    how: change the redux slice file, add fetching data with axios
    
    * [Feat] Make the CheckBoxes Clickable
    
    why: better ux
    how: add plugin and listen to changes
    
    * [Fix] Remove unwanted console.log

--- src/components/Common/MarkdownEditor/MarkdownEditor.jsx
+++ src/components/Common/MarkdownEditor/MarkdownEditor.jsx
@@ -9,18 +9,49 @@ import {
 import rehypeSanitize from "rehype-sanitize";
 import "./MarkdownEditor.css";
 
+const compareStrings = (str1, str2) => {
+    for (let i = 0, j = 0; i < str1.length || j < str2.length; i++, j++) {
+        if (i >= str1.length) return false;
+        if (j >= str2.length) return false;
+        if (str1[i] !== str2[j]) return false;
+    }
+    return true;
+};
 const MarkdownEditor = ({ content, label, previewModeOnly, onCopyChanges }) => {
-    const [value, setValue] = useState();
+    const [value, setValue] = useState("");
     useEffect(() => {
         setValue(content);
     }, [content, label]);
 
-    if (previewModeOnly)
-        return <MDEditor.Markdown source={value} style={{ whiteSpace: "normal", backgroundColor: "#000" }} />;
-
+    if (previewModeOnly) {
+        return (
+            <MDEditor.Markdown
+                source={value}
+                style={{ whiteSpace: "normal", backgroundColor: "#000" }}
+                components={{
+                    input: (props) => {
+                        return <input {...props} disabled={true} />;
+                    },
+                }}
+            />
+        );
+    }
     const handleChange = (value) => {
         setValue(value);
         onCopyChanges(label, value);
+    };
+
+    const handleCheckBoxChange = (e) => {
+        const textOfCheckBox = e.target.parentNode.textContent;
+        const valueListOfLines = value.split("\n");
+        const findCheckedBoxLineIndex = valueListOfLines.findIndex((item) =>
+            compareStrings(item?.replace(/- \[ \]|- \[[^]]+/, ""), textOfCheckBox.split("\n")[0]),
+        );
+        valueListOfLines[findCheckedBoxLineIndex] = valueListOfLines[findCheckedBoxLineIndex].replace(
+            /- \[ \]|- \[[^]]+/,
+            (match) => (match === "- [ ]" ? "- [X]" : "- [ ]"),
+        );
+        handleChange(valueListOfLines.join("\n"));
     };
     return (
         <MarkdownContainer>
@@ -32,6 +63,27 @@ const MarkdownEditor = ({ content, label, previewModeOnly, onCopyChanges }) => {
                         whiteSpace: "normal",
                         paddingLeft: "5px",
                         paddingRight: "5px",
+                    }}
+                    components={{
+                        input: (props) => {
+                            return (
+                                <input
+                                    {...props}
+                                    disabled={false}
+                                    onChange={(e) => {
+                                        if (props.type === "checkbox") {
+                                            const isChecked = e.target.hasAttribute("checked");
+                                            handleCheckBoxChange(e);
+                                            if (isChecked) {
+                                                e.target.removeAttribute("checked");
+                                            } else {
+                                                e.target.setAttribute("checked", "checked");
+                                            }
+                                        }
+                                    }}
+                                />
+                            );
+                        },
                     }}
                 />
             </MarkdownEditorPreviewContainer>
