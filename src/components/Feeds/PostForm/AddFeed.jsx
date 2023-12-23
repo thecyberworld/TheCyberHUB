f0b3c789aa7b64f45eb1d9b4ca4ebbecf6ea5aee
sumanjeet0012 <sumanjeet0012@gmail.com>
2023-12-24 00:56:24 +0530

    Poll feature added

--- src/components/Feeds/PostForm/AddFeed.jsx
+++ src/components/Feeds/PostForm/AddFeed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AuthPopup from "../../../pages/AuthPopup/AuthPopup";
 import AddPostTags from "./AddPostTags/AddPostTags";
+import AddPollTag from "./AddPostTags/AddPollTag";
 import {
     AddFeedCommentContainer,
     AddImage,
@@ -22,6 +23,84 @@ import { createFeed } from "../../../features/feeds/feedsSlice";
 import { toast } from "react-toastify";
 
 const AddPost = ({ showPostTags, userDetails }) => {
+    // fs.readFile('./data.json', 'utf-8', (err, data) => {
+    //     if (err) throw err;
+    //     const dummyData = JSON.parse(data);
+    //     console.log(json);
+    //   });
+    const data = [
+        { id: 0, votes: 9, text: "Option One" },
+        { id: 1, votes: 4, text: "Option Two" },
+        { id: 2, votes: 7, text: "Option Three" },
+        { id: 3, votes: 6, text: "Option Four" },
+    ];
+    // const resData = [
+    //     { text: "Answer 1", votes: 5 },
+    //     { text: "Answer 2", votes: 4 },
+    //     { text: "Answer 3", votes: 7 },
+    // ];
+    // function handleVotes(item, results) {
+    //     // Here you probably want to manage
+    //     // and return the modified data to the server.
+    // }
+    const question = "What is your favorite food?";
+    const [poll, setPoll] = useState({
+        question,
+        answers: data,
+    });
+
+    const calculateTotalVotes = (pollData) => {
+        let total = 0;
+
+        pollData.answers.forEach((answer) => {
+            total += answer.votes;
+        });
+
+        return total;
+    };
+    const totalVotes = calculateTotalVotes(poll);
+    const Percentage = (votes) => {
+        return ((votes / totalVotes) * 100).toFixed(2);
+    };
+    const [post, setPost] = useState("Feed");
+    const [vote, setVote] = useState("");
+    function handleVote(answer) {
+        // Update votes
+        // const nextAnswers = poll.answers.map((a) => (a.text === answer.text ? { ...a, votes: a.votes + 1 } : a));
+
+        if (vote === "") {
+            setPoll((prevPoll) => {
+                const nextAnswers = prevPoll.answers.map((a) => {
+                    if (a.id === answer.id) {
+                        return { ...a, votes: a.votes + 1 };
+                    } else {
+                        return a;
+                    }
+                });
+
+                return {
+                    ...prevPoll,
+                    answers: nextAnswers,
+                };
+            });
+            setVote(answer.id);
+            
+        } else if (vote === answer.id) {
+            answer.votes = answer.votes - 1;
+            setVote("");
+            
+        } else {
+            poll.answers.find((a) => a.id === vote).votes -= 1;
+            answer.votes = answer.votes + 1;
+            setVote(answer.id);
+            
+        }
+
+        // setPoll((prevPoll) => ({
+        //     ...prevPoll,
+        //     answers: nextAnswers,
+        // }));
+    }
     const dispatch = useDispatch();
     const textareaRef = useRef(null);
     const imageInputRef = useRef(null);
@@ -32,6 +111,7 @@ const AddPost = ({ showPostTags, userDetails }) => {
 
     const [content, setContent] = useState("");
     const [tags, setTags] = useState([]);
+    const [pollOptions, setPollOptions] = useState([]);
     const [files, setFiles] = useState([]);
     const [feedImages, setFeedImages] = useState([]);
     const [showAuthPopup, setShowAuthPopup] = useState(false);
@@ -49,6 +129,13 @@ const AddPost = ({ showPostTags, userDetails }) => {
         textarea.style.height = "auto"; // Reset height to recalculate scrollHeight
         textarea.style.height = `${textarea.scrollHeight}px`; // Set height to fit content
         setContent(textarea.value);
+    };
+
+    const handlePollChange = () => {
+        const textarea = textareaRef.current;
+        textarea.style.height = "auto"; // Reset height to recalculate scrollHeight
+        textarea.style.height = `${textarea.scrollHeight}px`; // Set height to fit content
+        setpollContent(textarea.value);
     };
 
     const handleImageChange = (e) => {
@@ -89,6 +176,18 @@ const AddPost = ({ showPostTags, userDetails }) => {
             return updatedFiles;
         });
     };
+
+    const [pollContent, setpollContent] = useState("");
+    const handleToggle = () => {
+        if (post === "Feed") setPost("Poll");
+        else setPost("Feed");
+    };
+    // const handlePollChange = () => {
+    //     const textarea = textareaRef.current;
+    //     textarea.style.height = "auto"; // Reset height to recalculate scrollHeight
+    //     textarea.style.height = `${textarea.scrollHeight}px`; // Set height to fit content
+    //     setpollContent(textarea.value);
+    // };
 
     const onSubmit = async (e) => {
         e.preventDefault();
@@ -154,79 +253,130 @@ const AddPost = ({ showPostTags, userDetails }) => {
 
     const userDetail = userDetails?.find((userDetail) => userDetail?.user === user?._id);
     const avatar = cdnContentImagesUrl("/user/" + (userDetail?.avatar || "avatarDummy.png"));
-
     return (
         <AddFeedCommentContainer>
             <LeftSection>
                 <PostHeaderImg src={avatar} alt="Profile picture" />
             </LeftSection>
             <RightSection>
-                <div>
-                    <FeedCommentInput
-                        ref={textareaRef}
-                        placeholder="What's on your mind?"
-                        value={content}
-                        onChange={handleChange}
-                    />
-                    <p
-                        style={{
-                            color:
-                                remainingCharacters <= 99 ? "#ff2525" : remainingCharacters <= 500 ? "#ff6b08" : "grey",
-                            width: "100%",
-                            textAlign: "right",
-                            fontSize: "12px",
-                            fontWeight: "bold",
-                            // border: '1px solid #ff6b08',
-                            marginTop: "-15px",
-                            marginBottom: "-10px",
-                        }}
-                    >
-                        {remainingCharacters < 0 ? "-" : ""} {Math.abs(remainingCharacters)}
-                    </p>
-                </div>
+                {post === "Feed" ? (
+                    <div>
+                        <div>
+                            <FeedCommentInput
+                                ref={textareaRef}
+                                placeholder="What's on your mind?"
+                                value={content}
+                                onChange={handleChange}
+                            />
+                            <p
+                                style={{
+                                    color:
+                                        remainingCharacters <= 99
+                                            ? "#ff2525"
+                                            : remainingCharacters <= 500
+                                            ? "#ff6b08"
+                                            : "grey",
+                                    width: "100%",
+                                    textAlign: "right",
+                                    fontSize: "12px",
+                                    fontWeight: "bold",
+                                    // border: '1px solid #ff6b08',
+                                    marginTop: "-15px",
+                                    marginBottom: "-10px",
+                                }}
+                            >
+                                {remainingCharacters < 0 ? "-" : ""} {Math.abs(remainingCharacters)}
+                            </p>
+                        </div>
 
-                <ImagesContainer>
-                    {files.map((file, index) => (
-                        <ImageContainer key={index}>
-                            <FeedImage src={URL.createObjectURL(file)} alt={`Uploaded ${index + 1}`} />
-                            <RemoveButton onClick={() => handleRemoveImage(index)}>&#10005;</RemoveButton>
-                        </ImageContainer>
-                    ))}
-                </ImagesContainer>
+                        <ImagesContainer>
+                            {files.map((file, index) => (
+                                <ImageContainer key={index}>
+                                    <FeedImage src={URL.createObjectURL(file)} alt={`Uploaded ${index + 1}`} />
+                                    <RemoveButton onClick={() => handleRemoveImage(index)}>&#10005;</RemoveButton>
+                                </ImageContainer>
+                            ))}
+                        </ImagesContainer>
 
-                {/* File input for image upload */}
-                {files.length < 4 && (
-                    <input
-                        type="file"
-                        name="feedImage"
-                        id="feedImage"
-                        ref={imageInputRef}
-                        onChange={handleImageChange}
-                        accept="image/*"
-                        multiple
-                        style={{ display: "none" }}
-                    />
+                        {/* File input for image upload */}
+                        {files.length < 4 && (
+                            <input
+                                type="file"
+                                name="feedImage"
+                                id="feedImage"
+                                ref={imageInputRef}
+                                onChange={handleImageChange}
+                                accept="image/*"
+                                multiple
+                                style={{ display: "none" }}
+                            />
+                        )}
+
+                        {showPostTags && <AddPostTags tags={tags} setTags={setTags} />}
+
+                        <FooterSection>
+                            <ImageUploadLabel
+                                style={{ background: "transparent", border: "transparent", padding: "0" }}
+                                htmlFor="feedImage"
+                            >
+                                {files.length < 4 && <AddImage />}
+                            </ImageUploadLabel>
+
+                            {isFeedLoading ? (
+                                <PostFormButton>
+                                    {" "}
+                                    <CircleSpinner size={17} />{" "}
+                                </PostFormButton>
+                            ) : (
+                                <PostFormButton onClick={onSubmit}>Create</PostFormButton>
+                            )}
+                        </FooterSection>
+                    </div>
+                ) : (
+                    <>
+                        <div className="flex flex-col">
+                            <FeedCommentInput
+                                ref={textareaRef}
+                                placeholder="What's your Question for Poll?"
+                                value={pollContent}
+                                onChange={handlePollChange}
+                            />
+                            <h3>{poll.question}</h3>
+                            <div className="flex flex-col">
+                                {<AddPollTag tags={pollOptions} setTags={setPollOptions} />}
+                            </div>
+                            <div className="border border-solid border-gray-400 border-1">
+                                {poll.answers.map((answer) => (
+                                    <div key={answer.id} className="flex flex-col w-full">
+                                        <button
+                                            className="relative mt-1 mb-1 px-4 py-2 text-white bg-black-600 rounded overflow-hidden"
+                                            onClick={() => handleVote(answer)}
+                                        >
+                                            <span className="relative z-10">
+                                                {answer.text} {Percentage(answer.votes)}%
+                                            </span>
+                                            <div
+                                                className="absolute inset-0 bg-blue-500"
+                                                style={{ width: `${Percentage(answer.votes)}%` }}
+                                            ></div>
+                                        </button>
+                                        <div className="flex flex-col w-full">
+                                            {/* <ProgressBar now={100} variant="success" label="label" />
+                                        <ProgressBar now={70} label="label2" visuallyHidden />
+                                        <ProgressBar now={now} label={`${now}%`} />
+                                        <AnimatedExample />
+                                        <Button variant="primary">Primary bootstrap button</Button>{' '} */}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </>
                 )}
 
-                {showPostTags && <AddPostTags tags={tags} setTags={setTags} />}
-
-                <FooterSection>
-                    <ImageUploadLabel
-                        style={{ background: "transparent", border: "transparent", padding: "0" }}
-                        htmlFor="feedImage"
-                    >
-                        {files.length < 4 && <AddImage />}
-                    </ImageUploadLabel>
-
-                    {isFeedLoading ? (
-                        <PostFormButton>
-                            {" "}
-                            <CircleSpinner size={17} />{" "}
-                        </PostFormButton>
-                    ) : (
-                        <PostFormButton onClick={onSubmit}>Create</PostFormButton>
-                    )}
-                </FooterSection>
+                <PostFormButton onClick={handleToggle}>
+                    {post === "Feed" ? "Create Poll" : "Create Feed"}
+                </PostFormButton>
             </RightSection>
 
             {/* Render the AuthPopup component */}
---
+++ src/components/Feeds/PostForm/AddPostTags/AddPollTag.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useRef, useState } from "react";
+import { IconAdd, IconRemove, TagInput, TagsInput, TagsInputContainer } from "./AddPostTagsElements";
+import { PostTag, PostTags } from "../../FeedPosts/FeedPostsElements";
+
+const AddPollTag = ({ tags, setTags, size }) => {
+    const [hideInput, setHideInput] = useState(true);
+    const [addTag, setAddTag] = useState("");
+
+    useEffect(() => {
+        if (tags.length > 9) {
+            setHideInput(false);
+        } else {
+            setHideInput(true);
+        }
+    }, [tags]);
+
+    const handleAddTag = () => {
+        if (addTag.trim() !== "") {
+            setTags([...tags, addTag.trim()]);
+            setAddTag("");
+        }
+    };
+
+    const handleRemoveTag = () => {
+        setTags((prevTags) => prevTags.slice(0, prevTags.length - 1));
+    };
+
+    const handleTagInputChange = (event) => {
+        const value = event.target.value;
+        setAddTag(value);
+
+        // Check if the user has entered a tag and pressed Enter or typed a comma
+        if (value.trim() !== "" && (event.key === "Enter" || event.key === ",")) {
+            setTags((prevTags) => [...prevTags, value.trim()]);
+            setAddTag("");
+        }
+    };
+
+    const handleTagInputBlur = () => {
+        const value = addTag.trim();
+        if (value !== "") {
+            setTags((prevTags) => [...prevTags, value]);
+            setAddTag("");
+        }
+    };
+    const handleAddTagOnEnter = (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            handleAddTag();
+        }
+    };
+
+    const tagInputRef = useRef(null);
+
+    useEffect(() => {
+        const tagInput = tagInputRef.current;
+        tagInput.style.width = "100%";
+        tagInput.style.width = `${tagInput.scrollWidth}px`;
+
+        if (!addTag) {
+            tagInput.style.width = size === "lg" ? "80px" : "60px";
+        }
+    }, [addTag]);
+
+    return (
+        <TagsInputContainer size={size}>
+            {tags.length !== 0 ? (
+                <PostTags size={size}>
+                    {tags.map(
+                        (tag, id) =>
+                            tag !== "" && (
+                                <PostTag size={size} key={id}>
+                                    {tag}
+                                </PostTag>
+                            ),
+                    )}
+                </PostTags>
+            ) : null}
+            {hideInput ? (
+                <TagsInput size={size}>
+                    <TagInput
+                        size={size}
+                        ref={tagInputRef}
+                        name="tags"
+                        id="tags"
+                        type="text"
+                        value={addTag}
+                        onChange={handleTagInputChange}
+                        onKeyDown={handleAddTagOnEnter}
+                        onBlur={handleTagInputBlur} // Add onBlur event handler
+                        placeholder="Add options"
+                    />
+                    <IconAdd onClick={handleAddTag}>+</IconAdd>
+                    {tags.length !== 0 ? (
+                        <>
+                            <IconRemove onClick={handleRemoveTag}>-</IconRemove>
+                        </>
+                    ) : null}
+                </TagsInput>
+            ) : null}
+        </TagsInputContainer>
+    );
+};
+
+export default AddPollTag;
