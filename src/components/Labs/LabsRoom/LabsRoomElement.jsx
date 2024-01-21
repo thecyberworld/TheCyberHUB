ac675ab16472c9641be81240276042f7057d0b4e
Saedbhati <105969318+Saedbhati@users.noreply.github.com>
2024-01-21 12:48:47 +0530

    feat: Labs and reverseShell fix (#610)
    * CreateQuiz
    
    * Fix ReverseShell
    
    * labs
    
    ---------
    
    Co-authored-by: Kabir <44284877+kabir0x23@users.noreply.github.com>

---
+++ src/components/Labs/LabsRoom/LabsRoomElement.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    padding: 10px 15px;
+    backgroundcolor: #ff6b08;
+    color: #151515;
+    border: none;
+    borderradius: 5px;
+    cursor: pointer;
+`;
