7124c4e84a7785bdcbb4f72610665125cf34f19a
Arkadi Koifman <76536506+ArkadiK94@users.noreply.github.com>
2024-01-25 23:29:05 +0200

    feat: manage community events | handle edit option (#614)
    * refactor: Change Status Options
    
    why: so there can be only Cancelled or Approved events
    how: change in relevant places
    
    * feat: handle editing event
    
    why: so admin could edit event info
    how: pass this event data to modify-event component
    
    * feat: handle editing event
    
    why: so admin could edit event info
    how: pass this event data to modify-event component

--- src/components/CommunityEvents/CommunityEvents.jsx
+++ src/components/CommunityEvents/CommunityEvents.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     ParentContainer,
     Container,
@@ -15,6 +15,19 @@ import { EventItemList } from "./EventItemList";
 import { RouterNavCreateButton } from "../Header/Navbar/NavbarElements";
 import ModifyCommunityEvent from "./ModifyCommunityEvent";
 
+const getTimeRelatedData = () => {
+    const today = new Date();
+    const todayString = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, "0")}-${String(
+        today.getDate(),
+    ).padStart(2, "0")}`;
+
+    const currentTime = `${today.getHours()}:${today.getMinutes()}`;
+
+    const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+    return [todayString, currentTime, daysOfWeek];
+};
+
 const CommunityEvents = ({
     events,
     pageHeader,
@@ -25,26 +38,66 @@ const CommunityEvents = ({
     eventsJoinedId = [],
     user,
     onActionChange = () => {},
+    modifyEventId,
+    setModifyEventId,
 }) => {
     const [isActiveTab, setActiveTab] = useState(0);
     const [openCreatingNewEvent, setOpenCreatingNewEvent] = useState(false);
-    const today = new Date();
-    const todayString = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, "0")}-${String(
-        today.getDate(),
-    ).padStart(2, "0")}`;
 
     const tabNames = [
         { id: 0, status: "upcoming" },
         { id: 1, status: "past" },
         { id: 2, status: "cancelled" },
     ];
+    const [todayString, currentTime, daysOfWeek] = getTimeRelatedData();
 
-    const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    const filteredEvents = events
+        .filter((event) => {
+            let eventFit = false;
+            switch (tabNames[isActiveTab].status) {
+                case "cancelled":
+                    if (event.status === tabNames[isActiveTab].status) eventFit = true;
+                    break;
+                case "past":
+                    if (
+                        event.status === "approved" &&
+                        (event.date < todayString || (event.date === todayString && event.endTime < currentTime))
+                    )
+                        eventFit = true;
+                    break;
+                case "upcoming":
+                    if (
+                        event.status === "approved" &&
+                        (event.date > todayString || (event.date === todayString && event.endTime >= currentTime))
+                    )
+                        eventFit = true;
+                    break;
+                default:
+                    eventFit = false;
+            }
+            return eventFit;
+        })
+        .sort((a, b) => {
+            return a.date < b.date || (a.date === b.date && a.startTime < b.startTime);
+        });
 
-    const filteredEvents = events.filter((event) => event.status === tabNames[isActiveTab].status);
-    const handleAddEvent = (newEvent) => {
-        events.push(newEvent);
+    const handleModifyEvent = (newEvent, eventId = "") => {
+        if (!eventId) return events.push(newEvent);
+        const modifiedEventIndex = events.findIndex((event) => event._id === eventId);
+        events[modifiedEventIndex] = { ...events[modifiedEventIndex], ...newEvent };
+        setModifyEventId("");
     };
+    useEffect(() => {
+        setOpenCreatingNewEvent(modifyEventId);
+        if (modifyEventId) {
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: "smooth",
+            });
+        }
+    }, [modifyEventId]);
+
     return (
         <ParentContainer pageHeader={pageHeader}>
             <Container>
@@ -73,18 +126,21 @@ const CommunityEvents = ({
                     {modify && openCreatingNewEvent && (
                         <ModifyCommunityEvent
                             setOpenCreatingNewEvent={setOpenCreatingNewEvent}
-                            onAdd={handleAddEvent}
+                            onModify={handleModifyEvent}
+                            modifyEvent={events.find((event) => event._id === modifyEventId)}
+                            setModifyEventId={setModifyEventId}
+                            modifyEventId={modifyEventId}
                         />
                     )}
                     {filteredEvents.length !== 0 ? (
                         filteredEvents.map((data, index) => {
                             const dateObject = new Date(data.date);
                             const dayName = daysOfWeek[dateObject.getDay()];
-
                             return (
                                 <EventItemList
                                     data={data}
                                     todayString={todayString}
+                                    currentTime={currentTime}
                                     dayName={dayName}
                                     actions={actions}
                                     key={index}
@@ -93,6 +149,7 @@ const CommunityEvents = ({
                                     eventsJoinedId={eventsJoinedId}
                                     user={user}
                                     onActionChange={onActionChange}
+                                    tabStatus={tabNames[isActiveTab].status}
                                 />
                             );
                         })
