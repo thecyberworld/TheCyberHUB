4292d72c5db33b60c0d83b862b5d191e3db1f40e
Sumanjeet <sumanjeet0012@gmail.com>
2024-02-13 14:46:51 +0530

    feat: blogs | blog filter option is added (#631)
    * feat: blog filter option is added
    
    * fix: some extra lines are removed
    
    * fix: margins are adjusted
    
    * fix: logged in users will only see filter option
    
    * fix: minor changes
    
    * refactor: userDetailsFunction is created.

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { blogReset, getAllBlogs } from "../../features/blogs/blogSlice";
 import { Wrapper } from "../Dashboard/Profile/ProfileElements";
@@ -11,8 +11,18 @@ import UnderMaintenance from "../Other/UnderMaintenance/UnderMaintenance";
 import apiStatus from "../../features/apiStatus";
 import BlogCards from "./BlogCard/BlogCards";
 import { getAllUserDetails, userDetailReset } from "../../features/userDetail/userDetailSlice";
+import { getFollowData, reset } from "../../../src/features/follow/followSlice";
+import BlogFilter from "./BlogFilter";
 
 const Blogs = () => {
+    const [selectedBlogs, setSelectedBlogs] = useState("all");
+
+    const { user } = useSelector((state) => state.auth);
+
+    const handleSelectedBlogs = (e) => {
+        setSelectedBlogs(e.target.value);
+    };
+
     const { isApiLoading, isApiWorking } = apiStatus();
     const dispatch = useDispatch();
 
@@ -21,22 +31,25 @@ const Blogs = () => {
         (state) => state.userDetail,
     );
 
+    const userId = user?._id;
+
+    const { followData } = useSelector((state) => state.followData);
     useEffect(() => {
         if (isBlogError) console.log(blogMessage);
         if (isUserDetailError) console.log(userDetailMessage);
-
+        if (userId) dispatch(getFollowData(userId));
         dispatch(getAllBlogs());
         dispatch(getAllUserDetails());
 
         return () => {
+            dispatch(reset());
             dispatch(blogReset());
             dispatch(userDetailReset());
         };
-    }, [dispatch, isBlogError, blogMessage, isUserDetailError, userDetailMessage]);
+    }, [dispatch, isBlogError, blogMessage, isUserDetailError, userDetailMessage, userId]);
 
-    const blogsData = blogs?.map((blog) => {
+    const userDetailsfunction = (blog) => {
         const userDetail = userDetails?.find((user) => user.user === blog.user);
-
         const { username, avatar, verified } = userDetail || {};
 
         return {
@@ -45,7 +58,19 @@ const Blogs = () => {
             avatar,
             verified,
         };
+    };
+
+    const blogsData = blogs?.map((blog) => {
+        return userDetailsfunction(blog);
     });
+
+    const followingBlogData = blogs?.map((blog) => {
+        if (!followData?.following?.includes(blog.user)) {
+            return null;
+        }
+        return userDetailsfunction(blog);
+    });
+    const filteredBlogs = followingBlogData?.filter((blog) => blog !== null);
 
     if (isBlogLoading || isUserDetailLoading || isApiLoading) {
         return (
@@ -76,12 +101,14 @@ const Blogs = () => {
             <BlogsContainer>
                 <BlogsSection>
                     <RouterNavCreateButtonLink to={"/dashboard/blogs/create"}>Create Blog</RouterNavCreateButtonLink>
+                    {user && <BlogFilter selectedBlogs={selectedBlogs} handleSelectedBlogs={handleSelectedBlogs} />}
+
                     <MiddleContainer>
                         {/* <div> */}
                         {/*    <LeftBlogSidebar /> */}
                         {/* <Tags tags={tags} /> */}
                         {/* </div> */}
-                        <BlogCards blogs={blogsData} />
+                        <BlogCards blogs={selectedBlogs === "all" ? blogsData : filteredBlogs} />
                     </MiddleContainer>
                 </BlogsSection>
             </BlogsContainer>
