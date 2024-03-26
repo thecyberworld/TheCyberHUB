b64b208a69ec5d431ab1e30194689e244267a13e
Daniel Okobia <87054930+duokobia@users.noreply.github.com>
2024-03-26 08:11:10 +0100

    fix: Center doughnut chart

--- src/components/Dashboard/Analytics/AnalyticsDoughnutChartElements.jsx
+++ src/components/Dashboard/Analytics/AnalyticsDoughnutChartElements.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const DoughnutChartContainer = styled.div`
-display: flex;
-justify-content: center; 
-height: 10.5rem;
-margin-top: -1.5rem
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 10.5rem;
+    margin-top: -1.5rem;
 `;
-
