df67649698b871456a6115a81f688450b33bc2cd
ArkadiK94 <arkadi29081994@gmail.com>
2024-04-18 23:28:58 +0300

    feat: add edit/delete options in feed options screen and close functionality
    why: so we will have those options, about the close so user could close
    the modal window

--- src/components/Feeds/FeedOptions/FeedOptions.jsx
+++ src/components/Feeds/FeedOptions/FeedOptions.jsx
@@ -1,16 +1,78 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import { FeedOptionsContainer } from "./FeedOptionsElements";
+import { TbEditCircle } from "react-icons/tb";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
+
+import {
+    FeedOptionsList,
+    FeedOptionsContainer,
+    FeedOptionsMainContainer,
+    FeedOptionsItem,
+    OptionLabel,
+    OptionButton,
+    FeedOptionsClose,
+} from "./FeedOptionsElements";
+import "./Feed.css";
 
 const FeedOptions = () => {
-    const handleClickEvent = (e) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const modalWindow = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (modalWindow.current && !modalWindow.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [modalWindow]);
+    const handleClickEvent = (e, option) => {
         e.stopPropagation();
         e.preventDefault();
+        setIsOpen(option);
     };
+
+    const handleDeleteFeed = () => {};
+    const handleEditFeed = () => {};
     return (
-        <FeedOptionsContainer onClick={handleClickEvent}>
+        <FeedOptionsMainContainer ref={modalWindow} isOpen={isOpen} onClick={(e) => handleClickEvent(e, true)}>
             <BsThreeDotsVertical />
-        </FeedOptionsContainer>
+            {isOpen && (
+                <FeedOptionsContainer>
+                    <FeedOptionsClose onClick={(e) => handleClickEvent(e, false)}>
+                        <MdClose className="icon icon-close" size="18px" title="Close" />
+                    </FeedOptionsClose>
+                    <FeedOptionsList>
+                        <FeedOptionsItem>
+                            <OptionLabel>Edit</OptionLabel>
+                            <OptionButton>
+                                <TbEditCircle
+                                    className="icon icon-edit"
+                                    size="18px"
+                                    title="Edit"
+                                    onClick={handleEditFeed}
+                                />
+                            </OptionButton>
+                        </FeedOptionsItem>
+                        <FeedOptionsItem>
+                            <OptionLabel>Delete</OptionLabel>
+                            <OptionButton>
+                                <AiTwotoneDelete
+                                    className="icon icon-delete"
+                                    size="18px"
+                                    title="Delete"
+                                    onClick={handleDeleteFeed}
+                                />
+                            </OptionButton>
+                        </FeedOptionsItem>
+                    </FeedOptionsList>
+                </FeedOptionsContainer>
+            )}
+        </FeedOptionsMainContainer>
     );
 };
 export default FeedOptions;
--- src/components/Feeds/FeedOptions/FeedOptionsElements.jsx
+++ src/components/Feeds/FeedOptions/FeedOptionsElements.jsx
@@ -1,7 +1,49 @@
 import styled from "styled-components";
 
-export const FeedOptionsContainer = styled.button`
+export const FeedOptionsMainContainer = styled.div`
     position: relative;
-    z-index: 100;
     padding: 5px;
+    &:hover {
+        transform: ${({ isOpen }) => !isOpen && "scale(1.1)"};
+    }
+    cursor: pointer;
+`;
+export const FeedOptionsContainer = styled.div`
+    width: 200px;
+    height: 160px;
+    background-color: #0e0e0e;
+    position: absolute;
+    right: 10px;
+    top: 25px;
+    border-radius: 10px;
+    border: 1px #3d3d3d solid;
+    box-shadow: 2px 2px 10px #333333;
+    padding: 20px 0;
+`;
+export const FeedOptionsList = styled.ul`
+    padding: 15px 20px;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+export const FeedOptionsItem = styled.li`
+    &:hover {
+        border-bottom: 0.5px #a1a1a1 solid;
+        opacity: 0.7;
+        transition: all 0.3s ease;
+    }
+    display: flex;
+    justify-content: space-between;
+`;
+export const OptionLabel = styled.p`
+    font-size: 20px;
+    color: #a1a1a1;
+`;
+export const OptionButton = styled.button``;
+
+export const FeedOptionsClose = styled.button`
+    position: absolute;
+    top: 5px;
+    right: 5px;
 `;
