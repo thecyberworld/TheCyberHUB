a65632f50b0ad28cf30e74fd732befe226078feb
Tamir Abutbul <1tamir198@gmail.com>
2024-04-27 15:29:31 +0300

    Display error when input is empty

--- src/components/Dashboard/OldDashbaord/GoalSetter/GoalForm.jsx
+++ src/components/Dashboard/OldDashbaord/GoalSetter/GoalForm.jsx
@@ -1,30 +1,48 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createGoal } from "src/features/goals/goalSlice";
+import { toast } from "react-toastify";
+import styled from "styled-components";
+
+const GoalInput = styled.input`
+    width: 100%;
+    padding: 10px;
+    border: 1px solid ${(props) => (props.error ? "red" : "#e6e6e6")};
+    border-radius: 5px;
+    margin-bottom: 10px;
+    font-family: inherit;
+`;
 
 const GoalForm = () => {
     const [text, setText] = useState("");
+    const [error, setError] = useState(false);
 
     const dispatch = useDispatch();
 
     const onSubmit = (e) => {
         e.preventDefault();
-        if (text.trim() === "") return;
+        if (text.trim() === "") {
+            toast("Cannot submit empty goal");
+            setError(true);
+            return;
+        }
 
         dispatch(createGoal({ text }));
+        setError(false);
         setText("");
     };
 
     return (
         <section className={"form"}>
             <form onSubmit={onSubmit}>
-                <div className={"form-group"}>
+                <div>
                     <label htmlFor="goal">Goal</label>
-                    <input
+                    <GoalInput
                         type="text"
                         name="text"
                         id="text"
                         value={text}
+                        error={error}
                         onChange={(e) => setText(e.target.value)}
                         placeholder="What's your goal?"
                         style={{ color: "#000" }}
@@ -33,7 +51,7 @@ const GoalForm = () => {
                 </div>
 
                 <div className="form-group">
-                    <button className={"btn btn-block"} type={"submit"}>
+                    <button className={`btn btn-block ${error}`} type={"submit"}>
                         Add Goal
                     </button>
                 </div>
