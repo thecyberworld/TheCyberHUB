b1255482fd7f3f7ca2084971ce0819fe92f6eeeb
Tamir Abutbul <1tamir198@gmail.com>
2024-05-06 22:50:24 +0300

    Display chart data in json

--- src/components/Dashboard/Analytics/AnalyticsMainBarChart.jsx
+++ src/components/Dashboard/Analytics/AnalyticsMainBarChart.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 import { BarChartContainer } from "./AnalyticsMainBarChartElements";
-import { displayMonths, allOptions as newOptions, allDatasets as newDatasets } from "./AnalyticsUtils";
 import { useAnalyticsChartCustomHook } from "./useAnalyticsChartCustomHook";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 
+import { options, datasets, displayMonths } from "./MainBarChartData.json";
+
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-export function AnalyticsMainBarChart() {
+export function AnalyticsMainBarChart({ timePeriod }) {
     const [chartData, chartOptions] = useAnalyticsChartCustomHook({
-        displayMonths: displayMonths(),
-        newDatasets: newDatasets.slice(-1),
-        newOptions: newOptions.slice(0, 1),
+        displayMonths,
+        newDatasets: datasets,
+        newOptions: options,
     });
 
     return (
