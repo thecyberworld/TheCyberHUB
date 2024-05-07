70415cac5db13f68af0ece7ca4aac0db971c9afa
Tamir Abutbul <1tamir198@gmail.com>
2024-05-07 17:56:30 +0300

    Adding data files for months and hours

--- src/components/Dashboard/Analytics/AnalyticsMainBarChart.jsx
+++ src/components/Dashboard/Analytics/AnalyticsMainBarChart.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Bar } from "react-chartjs-2";
 import { BarChartContainer } from "./AnalyticsMainBarChartElements";
 import { useAnalyticsChartCustomHook } from "./useAnalyticsChartCustomHook";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 
-import { options, datasets, displayMonths } from "./MainBarChartData.json";
+// import { options, datasets, displayMonths } from "./MainBarChartDataMonths.json";
+import { options, datasets, displayMonths } from "./MainBarChartDataHours.json";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export function AnalyticsMainBarChart({ timePeriod }) {
+    useEffect(() => {
+        fetch("./MainBarChartData.json")
+            .then((response) => response.json())
+            .then((data) => console.log({ data }));
+    }, []);
     const [chartData, chartOptions] = useAnalyticsChartCustomHook({
         displayMonths,
         newDatasets: datasets,
