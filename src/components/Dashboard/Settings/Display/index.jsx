fb02f2ea4959ee9256713931b90b7a3202408599
AhmedKarray005 <ahmedkarray902@gmail.com>
2024-05-19 15:13:34 +0100

    icon changes

--- src/components/Dashboard/Settings/Display/index.jsx
+++ src/components/Dashboard/Settings/Display/index.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons"; // Import search icon
+import { BiEdit, BiSearch, BiTrashAlt } from "react-icons/bi";
 import "./display.css";
 
-export default function Display() {
+export default function Login() {
     const [editing, setEditing] = useState(false);
     const [teamName, setTeamName] = useState("Change your Team Name");
     const [newTeamName, setNewTeamName] = useState("");
@@ -62,7 +61,7 @@ export default function Display() {
         event.preventDefault();
         setMembers([...members, newMember]);
         setNewMember({ name: "", email: "", activity: "", role: "" });
-        setShowForm(false);
+        setShowForm(false); // Hide the form after submitting
     };
 
     const handleInputChange = (event) => {
@@ -84,7 +83,7 @@ export default function Display() {
                     <>
                         <div className="team-name-container">
                             <h2 className="team-name-heading">{teamName}</h2>
-                            <FontAwesomeIcon icon={faEdit} className="edit-icon" onClick={handleEdit} />
+                            <BiEdit className="edit-icon" onClick={handleEdit} />
                         </div>
                     </>
                 )}
@@ -123,11 +122,7 @@ export default function Display() {
             </div>
             <div className="search-and-add">
                 <div className="group">
-                    <svg className="icon" aria-hidden="true" viewBox="0 0 24 24">
-                        <g>
-                            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-                        </g>
-                    </svg>
+                    <BiSearch className="icon" />
                     <input placeholder="Search" type="search" className="input" />
                 </div>
                 <button onClick={handleAddMemberClick} className="button type1">
@@ -167,7 +162,6 @@ export default function Display() {
                     </form>
                 )}
             </div>
-
             <div className="members-list">
                 <table className="members-table">
                     <thead>
@@ -187,11 +181,7 @@ export default function Display() {
                                 <td>{member.activity}</td>
                                 <td>{member.role}</td>
                                 <td>
-                                    <FontAwesomeIcon
-                                        icon={faTrashAlt}
-                                        className="delete-icon"
-                                        onClick={() => handleDelete(index)}
-                                    />
+                                    <BiTrashAlt className="delete-icon" onClick={() => handleDelete(index)} />
                                 </td>
                             </tr>
                         ))}
