fc2465219483555e5c12d57badd783ed01736bed
Tamir Abutbul <1tamir198@gmail.com>
2024-05-20 22:15:48 +0300

    Download demy file when clicking on the button

--- src/components/Dashboard/Analytics/AnalyticsUtils.js
+++ src/components/Dashboard/Analytics/AnalyticsUtils.js
@@ -142,3 +142,19 @@ export const allOptions = [
         responsive: true,
     },
 ];
+
+export const getAnalyticsFileData = () => {
+    const headers = [
+        { label: "First Name", key: "firstname" },
+        { label: "Last Name", key: "lastname" },
+        { label: "Email", key: "email" },
+    ];
+
+    const data = [
+        { firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
+        { firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
+        { firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" },
+    ];
+
+    return { headers, data };
+};
