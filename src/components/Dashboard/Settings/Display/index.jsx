56c4c2a37a608e04f3b096feaed86067ca532903
Tamir Abutbul <1tamir198@gmail.com>
2024-05-28 00:15:19 +0300

    Refactor css styles into styled modules

--- src/components/Dashboard/Settings/Display/index.jsx
+++ src/components/Dashboard/Settings/Display/index.jsx
@@ -1,6 +1,237 @@
 import React, { useState } from "react";
 import { BiEdit, BiSearch, BiTrashAlt } from "react-icons/bi";
-import "./display.css";
+import styled from "styled-components";
+
+const Section = styled.section`
+    width: 100%;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-bottom: 20px;
+    padding-bottom: 20px;
+`;
+
+const TeamName = styled.div`
+    display: flex;
+    align-items: center;
+    color: whitesmoke;
+`;
+
+const TeamNameContainer = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const TeamNameHeading = styled.h2`
+    font-size: 24px;
+    margin-right: 10px;
+`;
+
+const TeamNameInput = styled.input`
+    color: black;
+`;
+
+const EditIcon = styled(BiEdit)`
+    cursor: pointer;
+    color: #fffff0;
+    margin-bottom: 10px;
+
+    &:hover {
+        color: #0056b3;
+    }
+`;
+
+const OwnerContainer = styled.div`
+    background-color: #f5f7fa;
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 20px;
+`;
+
+const OwnerLabel = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+const OwnerNames = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+`;
+
+const OwnerNameContainer = styled.div`
+    border-color: #0d0c22;
+    border-width: 3px;
+    padding: 10px;
+    border-radius: 5px;
+    width: 10%;
+    color: black;
+    margin-right: 30px;
+    text-align: center;
+`;
+
+const SearchAndAdd = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Group = styled.div`
+    display: flex;
+    line-height: 28px;
+    align-items: center;
+    position: relative;
+    max-width: 190px;
+`;
+
+const Input = styled.input`
+    width: 100%;
+    height: 40px;
+    line-height: 28px;
+    padding: 0 1rem;
+    padding-left: 2.5rem;
+    border: 2px solid transparent;
+    border-radius: 8px;
+    outline: none;
+    background-color: #f3f3f4;
+    color: #0d0c22;
+    transition: 0.3s ease;
+
+    &::placeholder {
+        color: black;
+    }
+
+    &:focus,
+    &:hover {
+        outline: none;
+        border-color: rgb(234 76 137 / 40%);
+        background-color: #fff;
+        box-shadow: 0 0 0 4px rgb(234 76 137 / 10%);
+    }
+`;
+
+const Icon = styled(BiSearch)`
+    position: absolute;
+    left: 1rem;
+    fill: #9e9ea7;
+    width: 1rem;
+    height: 1rem;
+`;
+
+const AddMemberButton = styled.button`
+    height: 50px;
+    width: 200px;
+    position: relative;
+    background-color: transparent;
+    cursor: pointer;
+    border: 2px solid #252525;
+    overflow: hidden;
+    border-radius: 30px;
+    color: white;
+    transition: all 0.5s ease-in-out;
+
+    &:hover&::after {
+        visibility: visible;
+        transform: scale(100) translateX(2px);
+    }
+
+    &::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+        transition: all 0.5s ease-in-out;
+        background-color: #333;
+        border-radius: 30px;
+        visibility: hidden;
+        height: 10px;
+        width: 10px;
+        z-index: -1;
+    }
+`;
+
+const BtnTxt = styled.span`
+    z-index: 1;
+    font-weight: 800;
+    letter-spacing: 4px;
+`;
+
+const MembersTable = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+`;
+
+const TableHeader = styled.tr`
+    th {
+        background-color: #f0f0f0;
+        color: black;
+        border-bottom: 1px solid #ccc;
+        padding: 10px;
+        text-align: left;
+    }
+`;
+
+const TableRow = styled.tr`
+    td {
+        border-bottom: 1px solid #ccc;
+        padding: 10px;
+        text-align: left;
+        color: black;
+
+        &:first-child {
+            border-left: none;
+        }
+
+        &:last-child {
+            border-right: none;
+        }
+    }
+`;
+
+const DeleteIcon = styled(BiTrashAlt)`
+    color: #888;
+    cursor: pointer;
+
+    &:hover {
+        color: red;
+    }
+`;
+
+const TeamOwner = styled.div`
+    font-size: 20px;
+    font-weight: bold;
+`;
+
+const AddOwnerButton = styled.button`
+    font-size: 20px;
+    font-weight: bold;
+    margin-left: 10px;
+    color: darkslategrey;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+
+    input {
+        margin-bottom: 10px;
+        padding: 8px;
+        border-radius: 5px;
+        color: black;
+        border: 1px solid #ccc;
+    }
+
+    button {
+        padding: 8px;
+        border: none;
+        background-color: #333;
+        color: white;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+`;
 
 export default function Login() {
     const [editing, setEditing] = useState(false);
@@ -61,7 +292,7 @@ export default function Login() {
         event.preventDefault();
         setMembers([...members, newMember]);
         setNewMember({ name: "", email: "", activity: "", role: "" });
-        setShowForm(false); // Hide the form after submitting
+        setShowForm(false);
     };
 
     const handleInputChange = (event) => {
@@ -69,38 +300,24 @@ export default function Login() {
     };
 
     return (
-        <section className="section">
-            <div className="team-name">
+        <Section>
+            <TeamName>
                 {editing ? (
-                    <input
-                        type="text"
-                        value={newTeamName}
-                        onChange={handleChange}
-                        autoFocus
-                        className="team-name-input"
-                    />
+                    <TeamNameInput type="text" value={newTeamName} onChange={handleChange} autoFocus />
                 ) : (
-                    <>
-                        <div className="team-name-container">
-                            <h2 className="team-name-heading">{teamName}</h2>
-                            <BiEdit className="edit-icon" onClick={handleEdit} />
-                        </div>
-                    </>
+                    <TeamNameContainer>
+                        <TeamNameHeading>{teamName}</TeamNameHeading>
+                        <EditIcon onClick={handleEdit} />
+                    </TeamNameContainer>
                 )}
-                {editing && (
-                    <button className="save-button" onClick={handleSave}>
-                        Save
-                    </button>
-                )}
-            </div>
-            <div className="owner-container">
-                <div className="owner-label">
-                    <div className="team-owner">Team Owner: </div>
-                    <button className="add-owner-button" onClick={handleAddOwnerClick}>
-                        +
-                    </button>
+                {editing && <button onClick={handleSave}>Save</button>}
+            </TeamName>
+            <OwnerContainer>
+                <OwnerLabel>
+                    <TeamOwner>Team Owner: </TeamOwner>
+                    <AddOwnerButton onClick={handleAddOwnerClick}>+</AddOwnerButton>
                     {showOwnerForm && (
-                        <form onSubmit={handleAddOwner}>
+                        <Form onSubmit={handleAddOwner}>
                             <input
                                 name="owner"
                                 value={newOwner}
@@ -109,27 +326,25 @@ export default function Login() {
                                 required
                             />
                             <button type="submit">Add owner</button>
-                        </form>
+                        </Form>
                     )}
-                </div>
-                <div className="owner-names">
+                </OwnerLabel>
+                <OwnerNames>
                     {teamOwners.map((owner, index) => (
-                        <div key={index} className="owner-name-container">
-                            {owner}
-                        </div>
+                        <OwnerNameContainer key={index}>{owner}</OwnerNameContainer>
                     ))}
-                </div>
-            </div>
-            <div className="search-and-add">
-                <div className="group">
-                    <BiSearch className="icon" />
-                    <input placeholder="Search" type="search" className="input" />
-                </div>
-                <button onClick={handleAddMemberClick} className="button type1">
-                    <span className="btn-txt">+ Add Member</span>
-                </button>
+                </OwnerNames>
+            </OwnerContainer>
+            <SearchAndAdd>
+                <Group>
+                    <Icon />
+                    <Input placeholder="Search" type="search" />
+                </Group>
+                <AddMemberButton onClick={handleAddMemberClick} className="button type1">
+                    <BtnTxt>+ Add Member</BtnTxt>
+                </AddMemberButton>
                 {showForm && (
-                    <form onSubmit={handleAddMember}>
+                    <Form onSubmit={handleAddMember}>
                         <input
                             name="name"
                             value={newMember.name}
@@ -159,35 +374,33 @@ export default function Login() {
                             required
                         />
                         <button type="submit">Submit</button>
-                    </form>
+                    </Form>
                 )}
-            </div>
-            <div className="members-list">
-                <table className="members-table">
-                    <thead>
-                        <tr className="table-header">
-                            <th>Member Name</th>
-                            <th>Email</th>
-                            <th>Last Activity</th>
-                            <th>Role</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {members.map((member, index) => (
-                            <tr key={index}>
-                                <td>{member.name}</td>
-                                <td>{member.email}</td>
-                                <td>{member.activity}</td>
-                                <td>{member.role}</td>
-                                <td>
-                                    <BiTrashAlt className="delete-icon" onClick={() => handleDelete(index)} />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </section>
+            </SearchAndAdd>
+            <MembersTable>
+                <thead>
+                    <TableHeader>
+                        <th>Member Name</th>
+                        <th>Email</th>
+                        <th>Last Activity</th>
+                        <th>Role</th>
+                        <th>Action</th>
+                    </TableHeader>
+                </thead>
+                <tbody>
+                    {members.map((member, index) => (
+                        <TableRow key={index}>
+                            <td>{member.name}</td>
+                            <td>{member.email}</td>
+                            <td>{member.activity}</td>
+                            <td>{member.role}</td>
+                            <td>
+                                <DeleteIcon onClick={() => handleDelete(index)} />
+                            </td>
+                        </TableRow>
+                    ))}
+                </tbody>
+            </MembersTable>
+        </Section>
     );
 }
