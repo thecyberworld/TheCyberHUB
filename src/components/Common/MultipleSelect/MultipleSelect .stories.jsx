b169bbea27f71ccd46693481d73205bfa3fd828a
Tamir Abutbul <1tamir198@gmail.com>
2024-05-29 22:23:03 +0300

    MultipleSelect story

---
+++ src/components/Common/MultipleSelect/MultipleSelect .stories.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import MultipleSelect from "./MultipleSelect";
+import { Option, SelectContainer } from "./MultipleSelectElements";
+
+export default {
+    title: "Components/MultipleSelect/MultipleSelect",
+    component: MultipleSelect,
+};
+
+const Template = ({ optionsValuesWithLabels, ...args }) => (
+    <MultipleSelect {...args} optionsValuesWithLabels={optionsValuesWithLabels}>
+        <SelectContainer>
+            <select>
+                {optionsValuesWithLabels.map((option) => (
+                    <Option key={option.value} value={option.value}>
+                        {option.label}
+                    </Option>
+                ))}
+            </select>
+        </SelectContainer>
+    </MultipleSelect>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+    selectLabel: "Select an option",
+    selectedValue: "",
+    defaultValue: "",
+    optionsValuesWithLabels: [
+        { value: "1", label: "Option 1" },
+        { value: "2", label: "Option 2" },
+        { value: "3", label: "Option 3" },
+    ],
+};
