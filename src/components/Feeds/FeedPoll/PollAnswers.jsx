df51b44330d12abbca50f992bc8952d9417763cf
Tamir Abutbul <1tamir198@gmail.com>
2024-06-05 21:21:07 +0300

    Add Poll Answers

---
+++ src/components/Feeds/FeedPoll/PollAnswers.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+import { PollImage } from "./PollImage";
+
+const StyledAnswer = styled.div`
+    margin-bottom: 1rem;
+`;
+
+const PercentageLabel = styled.div`
+    margin-right: 10px;
+    width: 2.5rem;
+    text-align: right;
+    color: #ccc;
+`;
+
+const AnswerContainer = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const AnswerBarContainer = styled.div`
+    flex: 1;
+    margin-left: 1rem;
+`;
+
+const AnswerBar = styled.div`
+    background-color: ${({ isCorrectAnswer }) => (isCorrectAnswer ? "#4caf50" : "#d32f2f")};
+    height: 10px;
+    border-radius: 15px;
+    width: ${({ percentage }) => `${percentage}%`};
+`;
+
+const Title = styled.p`
+    font-weight: bold;
+    margin: 0;
+    color: #fff;
+`;
+
+const TitleContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 5px;
+`;
+
+const StyledPollImage = styled(PollImage)`
+    margin-left: 5px;
+`;
+
+export const PollAnswers = ({ answers }) => {
+    return answers.map(({ percentage, title, isCorrectAnswer }, index) => (
+        <StyledAnswer key={index}>
+            <AnswerContainer>
+                <PercentageLabel>{percentage}%</PercentageLabel>
+                <AnswerBarContainer>
+                    <TitleContainer>
+                        <Title>{title}</Title>
+                        {isCorrectAnswer && <StyledPollImage />} {/* Image after the title */}
+                    </TitleContainer>
+                    <AnswerBar isCorrectAnswer={isCorrectAnswer} percentage={percentage} />
+                </AnswerBarContainer>
+            </AnswerContainer>
+        </StyledAnswer>
+    ));
+};
---
+++ src/components/Feeds/FeedPoll/PollQuestions.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import { StyledCheckbox } from "./StyledCheckbox";
+
+const StyledQuestion = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding: 0.2rem;
+    color: #fff;
+`;
+
+export const PollQuestions = ({ questions }) => {
+    return questions.map((question, index) => {
+        return (
+            <StyledQuestion key={index}>
+                <StyledCheckbox />
+                <p>{question}</p>
+            </StyledQuestion>
+        );
+    });
+};
