585083c3060ff036987f3909d8d15d11133541e9
Kabir <44284877+kabir0x23@users.noreply.github.com>
2024-09-06 20:09:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,14 +18,8 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            // "/api": "http://localhost:5000",
-            // "/api": "https://api.thecyberhub.org",
             "/api": {
                 target: apiUrl,
-                // target: "https://api.thecyberhub.org",
-                // target: "https://api-thecyberhub-org.azurewebsites.net",
-                // target: "https://alpha-testing-please-do-not-hack-us-1.thecyberhub.org",
-                // target: "https://dev.api.thecyberhub.org",
                 changeOrigin: true,
                 secure: false,
             },
@@ -33,9 +27,6 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 3000,
     },
-    // preview: {
-    //     port: 8080
-    // },
     plugins: [react()],
     build: {
         rollupOptions: {
